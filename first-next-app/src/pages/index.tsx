4e3c35f657851355b678f12f7b79070243a6650f
kmww <kmw9708@gmail.com>
2023-07-06 16:19:12 +0900

    chore: app폴더 제거

---
+++ first-next-app/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <title>first-next-app</title>
+      </Head>
+      <main>homepage</main>
+    </>
+  );
+}

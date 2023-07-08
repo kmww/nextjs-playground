73747f9c19df408bc5f49be52317f43e7eff329b
kmww <kmw9708@gmail.com>
2023-07-08 21:20:40 +0900

    next Image

--- first-next-app/src/pages/blog/[date]/[title].tsx
+++ first-next-app/src/pages/blog/[date]/[title].tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps, NextPage } from 'next';
+import Image from 'next/image';
 
 interface DetailPostProps {
   date: string;
@@ -21,6 +22,12 @@ const DetailPost: NextPage<DetailPostProps> = (props) => {
   return (
     <>
       <h1>{props.title}</h1>
+      <Image
+        src='https://images.unsplash.com/photo-1605460375648-278bcbd579a6'
+        width={500}
+        height={200}
+        alt='setter'
+      />
       <div>{props.date}</div>
     </>
   );

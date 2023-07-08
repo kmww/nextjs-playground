11a1802e5ab81e2befe6f887353b4b3a8cd040c0
kmww <kmw9708@gmail.com>
2023-07-08 19:56:50 +0900

    href  객체 전달

--- first-next-app/src/pages/blog/index.tsx
+++ first-next-app/src/pages/blog/index.tsx
@@ -4,7 +4,13 @@ import Link from 'next/link';
 const BlogPage: NextPage = () => {
   return (
     <div>
-      <Link href='/blog/2023-01-01/new year'>Read First Post</Link>
+      <Link
+        href={{
+          pathname: '/blog/[date]/[title]',
+          query: { date: '2023-01-01', title: 'new year' },
+        }}>
+        Read First Post
+      </Link>
       <br />
       <Link href='/blog/2023-03-15/deadline'>Read Second Post</Link>
     </div>

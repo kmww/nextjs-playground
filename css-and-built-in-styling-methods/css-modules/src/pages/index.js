399d284d1e129293a34b424c0ca51f27273a0015
kmww <kmw9708@gmail.com>
2023-07-14 12:57:09 +0900

    composes 속성 사용

--- css-and-built-in-styling-methods/css-modules/src/pages/index.js
+++ css-and-built-in-styling-methods/css-modules/src/pages/index.js
@@ -2,7 +2,7 @@ import styles from '@/styles/Home.module.css';
 
 const Home = () => {
   return (
-    <div className={styles.homepage}>
+    <div className={`${styles.homepage} ${styles['homepage-red']}`}>
       <h1 className={styles.title}> CSS Modules example </h1>
     </div>
   );

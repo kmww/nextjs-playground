1bd4bf1495e7064fcb57d5d44e9390ad943c13ca
kmww <kmw9708@gmail.com>
2023-07-14 12:40:48 +0900

    css module 사용

---
+++ css-and-built-in-styling-methods/css-modules/src/pages/index.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.homepage}>
+      <h1 className={styles.title}> CSS Modules example </h1>
+    </div>
+  );
+};
+
+export default Home;

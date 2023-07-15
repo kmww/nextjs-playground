cb35a36c34f249c53ad91f18196f954227a3d3a6
kmww <kmw9708@gmail.com>
2023-07-15 17:18:45 +0900

    UserCard 구현

---
+++ ui-frameworks/acme-with-chakra/src/components/UserCard/index.tsx
@@ -0,0 +1,40 @@
+import { UserType } from '@/types/dataTypes';
+import {
+  Avatar,
+  Box,
+  Center,
+  Text,
+  VStack,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { ReactElement } from 'react';
+
+const UserCard = (props: UserType): ReactElement => {
+  return (
+    <Link href={`/user/${props.username}`} passHref>
+      <>
+        <VStack
+          spacing='4'
+          borderRadius='md'
+          boxShadow='xl'
+          padding='5'
+          backgroundColor={useColorModeValue('gray.50', 'gray.700')}>
+          <Center>
+            <Avatar size='lg' src={props.avatar} />
+          </Center>
+          <Center>
+            <Box textAlign='center'>
+              <Text fontWeight='bold' fontSize='xl'>
+                {props.first_name} {props.last_name}
+              </Text>
+              <Text fontSize='xs'> {props.job_title}</Text>
+            </Box>
+          </Center>
+        </VStack>
+      </>
+    </Link>
+  );
+};
+
+export default UserCard;
---
+++ ui-frameworks/acme-with-chakra/src/types/dataTypes.ts
@@ -0,0 +1,10 @@
+export interface UserType {
+  id: string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  description: string;
+  job_title: string;
+  avatar: string;
+  cover_image: string;
+}

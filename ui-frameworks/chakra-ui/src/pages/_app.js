6dafed38d4dde65ff686ee5f1c41bd9d1285e0c2
kmww <kmw9708@gmail.com>
2023-07-15 15:14:26 +0900

    customTheme 사용

--- ui-frameworks/chakra-ui/src/pages/_app.js
+++ ui-frameworks/chakra-ui/src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const customTheme = extendTheme({
+  colors: {
+    brand: {
+      100: '#ffebee',
+      200: '#e57373',
+      300: '#f44336',
+      400: '#e53935',
+    },
+  },
+});
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
+};
+
+export default App;

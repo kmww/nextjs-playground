2a4661b5079a3afe086cc2013a1e7ef2ebd1c049
kmww <kmw9708@gmail.com>
2023-07-15 17:26:53 +0900

    직원 정보 페이지

--- ui-frameworks/acme-with-chakra/src/pages/index.tsx
+++ ui-frameworks/acme-with-chakra/src/pages/index.tsx
@@ -1,5 +1,31 @@
+import UserCard from '@/components/UserCard';
+import users from '@/data/users';
+import { Box, Grid, GridItem, Text } from '@chakra-ui/react';
+
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <Box>
+        <Text
+          fontSize='xxx-large'
+          fontWeight='extrabold'
+          textAlign='center'
+          marginTop={9}>
+          ACME Corporation Employees
+        </Text>
+        <Grid
+          gridTemplateColumns={['1fr', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}
+          gridGap={10}
+          padding={10}>
+          {users.map((user) => (
+            <GridItem key={user.id}>
+              <UserCard {...user} />
+            </GridItem>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
 };
 
 export default Home;

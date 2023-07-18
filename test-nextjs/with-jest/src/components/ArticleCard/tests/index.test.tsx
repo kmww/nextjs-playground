737bbd709a146557f31996f6f0a60c2bacc3707c
kmww <kmw9708@gmail.com>
2023-07-18 20:55:12 +0900

    ArticleCard 100글자 테스트

--- test-nextjs/with-jest/src/components/ArticleCard/tests/index.test.tsx
+++ test-nextjs/with-jest/src/components/ArticleCard/tests/index.test.tsx
@@ -1,11 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import ArticleCard from '../';
 import { article } from './mock';
+import { cutTextToLength } from '@/utils';
 
 describe('ArticleCard', () => {
   test('Generated link should be in the correct format', () => {
     const component = render(<ArticleCard {...article} />);
     const link = component.getByRole('link').getAttribute('href');
     expect(link).toBe('/articles/healthy-summer-meloncarrot-soup-u12w3o0d');
+  });
+
+  test('Generated summary should not exceed 100 characters', async () => {
+    render(<ArticleCard {...article} />);
+    const summary = screen.getByText(cutTextToLength(article.body, 100));
+    expect(summary).toBeDefined();
   });
 });

5b4f7393577fb53a2e22340ff58f6d312e23b583
kmww <kmw9708@gmail.com>
2023-07-19 17:58:33 +0900

    article key값 테스트

--- test-nextjs/with-cypress/cypress/e2e/intergration/api.cy.ts
+++ test-nextjs/with-cypress/cypress/e2e/intergration/api.cy.ts
@@ -11,4 +11,26 @@ describe('articles APIs', () => {
       .its('status')
       .should('be.equal', 200);
   });
+
+  it('should correctly return a list of articles', (done) => {
+    cy.request('http://localhost:3000/api/articles')
+      .its('body')
+      .each((article: Record<string, any>) => {
+        expect(article).to.have.keys('id', 'title', 'body', 'author', 'image');
+        expect(article.author).to.have.keys('id', 'name');
+        expect(article.image).to.have.keys('url', 'author');
+        done();
+      });
+  });
+
+  it('should correctly return a an article given an ID', (done) => {
+    cy.request('http://localhost:3000/api/article?id=u12w3o0d').then(
+      ({ body }) => {
+        expect(body).to.have.keys('id', 'title', 'body', 'author', 'image');
+        expect(body.author).to.have.keys('id', 'name');
+        expect(body.image).to.have.keys('url', 'author');
+        done();
+      }
+    );
+  });
 });

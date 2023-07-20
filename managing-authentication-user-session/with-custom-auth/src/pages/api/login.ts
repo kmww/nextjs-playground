fdcc881add8ca489e68187f08b2a9ee614ebdfdc
kmww <kmw9708@gmail.com>
2023-07-20 17:00:24 +0900

    custom-auth template

---
+++ managing-authentication-user-session/with-custom-auth/src/pages/api/login.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const loginApi = (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  const { email, password } = req.body;
+
+  if (method !== 'POST') {
+    return res.status(404).end();
+  }
+};
+
+export default loginApi;

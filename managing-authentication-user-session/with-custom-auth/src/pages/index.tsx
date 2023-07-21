a700b91a33a38c784432ab8531f73000d610c674
kmww <kmw9708@gmail.com>
2023-07-21 13:37:16 +0900

    로그인 버튼

--- managing-authentication-user-session/with-custom-auth/src/pages/index.tsx
+++ managing-authentication-user-session/with-custom-auth/src/pages/index.tsx
@@ -1,5 +1,15 @@
+import styles from '@/styles/app.module.css';
+import { useRouter } from 'next/router';
+
 const Home = () => {
-  return <>Home</>;
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <h1>homepage</h1>
+      <button onClick={() => router.push('/login')}>Login</button>
+    </div>
+  );
 };
 
 export default Home;

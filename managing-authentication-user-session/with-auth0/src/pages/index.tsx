e46a44be2f3a229b608dcc2d4653590a937283ba
kmww <kmw9708@gmail.com>
2023-07-25 03:10:59 +0900

    메인페이지 확인

--- managing-authentication-user-session/with-auth0/src/pages/index.tsx
+++ managing-authentication-user-session/with-auth0/src/pages/index.tsx
@@ -1,5 +1,34 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from 'next/link';
+
 const Home = () => {
-  return <></>;
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (user) {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+        <p>You're logged in with the following email: {user.email}</p>
+        <Link href='/api/auth/logout'>Logout</Link>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Welcome!, please Login</h1>
+      <Link href='api/auth/login'>Login</Link>
+    </div>
+  );
 };
 
 export default Home;

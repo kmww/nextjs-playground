f7c566026d92a4e92ccf6283228357ed813e8287
kmww <kmw9708@gmail.com>
2023-07-29 15:02:54 +0900

    상품 상세 데이터

--- building-ecommerce/src/pages/product/[slug].tsx
+++ building-ecommerce/src/pages/product/[slug].tsx
@@ -1,7 +1,8 @@
-import { GetStaticPaths } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import graphql from '@/graphql';
 import getAllProducts from '@/graphql/queries/getAllProducts';
 import { ProductsType } from '@/types/product';
+import getProductDetail from '@/graphql/queries/getProductDetail';
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const { products }: ProductsType = await graphql.request(getAllProducts);
@@ -18,7 +19,23 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps = async () => {};
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  if (!params) {
+    return {
+      props: {},
+    };
+  }
+
+  const { products }: ProductsType = await graphql.request(getProductDetail, {
+    slug: params.slug,
+  });
+
+  return {
+    props: {
+      product: products[0],
+    },
+  };
+};
 
 const ProductPage = () => {};
 

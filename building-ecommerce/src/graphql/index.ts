af959fbf4f197ffb80a103f6b1735391a4938189
kmww <kmw9708@gmail.com>
2023-08-02 11:49:15 +0900

    장바구니 제품 정보 가져오기

--- building-ecommerce/src/graphql/index.ts
+++ building-ecommerce/src/graphql/index.ts
@@ -1,6 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
-const { GRAPHCMS_ENDPOINT= '', GRAPHCMS_API_KEY = null } = process.env;
+const GRAPHCMS_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT as string;
+const GRAPHCMS_API_KEY = process.env.GRAPHCMS_API_KEY;
 const authorization = `Bearer ${GRAPHCMS_API_KEY}`;
 
 const GraphqlClient = new GraphQLClient(GRAPHCMS_ENDPOINT, {
--- building-ecommerce/src/pages/cart.tsx
+++ building-ecommerce/src/pages/cart.tsx
@@ -1,9 +1,26 @@
 import CartContext from '@/context/Cart';
+import graphql from '@/graphql';
+import getProductsById from '@/graphql/queries/getProductsById';
+import { ProductsType } from '@/types/product';
 import { Box, Divider, Text } from '@chakra-ui/react';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 const CartPage = () => {
   const { items } = useContext(CartContext);
+  const [products, setProducts] = useState<ProductsType>();
+  const hasProducts = Object.keys(items).length;
+
+  useEffect(() => {
+    if (!hasProducts) return;
+    graphql
+      .request<{ products: ProductsType }>(getProductsById, {
+        ids: Object.keys(items),
+      })
+      .then((data) => {
+        setProducts(data.products);
+      })
+      .catch((err) => console.error(err));
+  }, [hasProducts, items]);
 
   return (
     <Box rounded='xl' boxShadow='2xl' w='container.lg' p='16' bgColor='white'>

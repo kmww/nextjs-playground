534b3783600b35d2e253f9aac4766bd995a5b081
kmww <kmw9708@gmail.com>
2023-08-02 12:00:48 +0900

    getTotalCharge 함수

---
+++ building-ecommerce/src/utils/getTotalCharge.ts
@@ -0,0 +1,17 @@
+import { ProductsType } from '@/types/product';
+
+export const getTotalCharge = (
+  items: Record<string, any>,
+  { products }: ProductsType
+) => {
+  if (!products.length) return 0;
+
+  return Object.keys(items)
+    .map(
+      (id) =>
+        (products.find((product) => product.id === id)?.price as number) *
+        ((items[id] / 100) * 1.1)
+    )
+    .reduce((acc, cur) => acc + cur)
+    .toFixed(2);
+};

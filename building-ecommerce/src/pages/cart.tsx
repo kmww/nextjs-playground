d78f2281e339fb2368db0c269d40f04b7c3bb399
kmww <kmw9708@gmail.com>
2023-08-02 11:25:23 +0900

    get product by id query

--- building-ecommerce/src/pages/cart.tsx
+++ building-ecommerce/src/pages/cart.tsx
@@ -1,6 +1,10 @@
+import CartContext from '@/context/Cart';
 import { Box, Divider, Text } from '@chakra-ui/react';
+import { useContext } from 'react';
 
 const CartPage = () => {
+  const { items } = useContext(CartContext);
+
   return (
     <Box rounded='xl' boxShadow='2xl' w='container.lg' p='16' bgColor='white'>
       <Text as='h1' fontSize='2xl' fontWeight='bold'>

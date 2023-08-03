218d6dcfab8600011fb52b1eddca04c65e703438
kmww <kmw9708@gmail.com>
2023-08-03 16:58:13 +0900

    stripe 제품 상세 정보 가져오기

--- building-ecommerce/src/pages/api/checkout.ts
+++ building-ecommerce/src/pages/api/checkout.ts
@@ -1,10 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server';
+import graphql from '@/graphql';
+import getProductDetailsById from '@/graphql/queries/getProductDetailsById';
+import { ProductType, ProductsType } from '@/types/product';
+import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2022-11-15',
 });
 
-const checkout = (req: NextRequest, res: NextResponse) => {};
+const checkout = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { items } = req.body;
+  const { products } = await graphql.request<ProductsType>(
+    getProductDetailsById,
+    {
+      ids: Object.keys(items),
+    }
+  );
+};
 
 export default checkout;

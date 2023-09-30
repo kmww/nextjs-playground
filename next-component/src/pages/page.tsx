575f372d60b309ba2f605c6caca3fbb24bd1caf1
kmw <kmw9708@gmail.com>
2023-09-30 14:18:05 +0900

    styledButton using props

--- next-component/src/pages/page.tsx
+++ next-component/src/pages/page.tsx
@@ -1,6 +1,11 @@
 import { NextPage } from 'next';
 import styled from 'styled-components';
 
+interface ButtonProps {
+  color: string;
+  backgroundColor: string;
+}
+
 const Badge = styled.span`
   padding: 8px 16px;
   font-weight: bold;
@@ -10,8 +15,29 @@ const Badge = styled.span`
   border-radius: 16px;
 `;
 
+const Button = styled.button<ButtonProps>`
+  color: ${(props) => props.color};
+  background: ${(props) => props.backgroundColor};
+  border: 2px solid ${(props) => props.color};
+  font-size: 2em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 8px;
+  cursor: pointer;
+`;
+
 const Page: NextPage = () => {
-  return <Badge>Hello Badge</Badge>;
+  return (
+    <div>
+      <Badge>Hello Badge</Badge>
+      <Button backgroundColor='transparent' color='#FF0000'>
+        Hello
+      </Button>
+      <Button backgroundColor='#1E90FF' color='#FFFFFF'>
+        Button
+      </Button>
+    </div>
+  );
 };
 
 export default Page;

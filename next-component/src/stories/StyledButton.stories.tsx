7aff63ff98e6f65f2e2e6355ddca37b9347e6769
kmw <kmw9708@gmail.com>
2023-10-05 19:38:19 +0900

    buttonComponent: addon-actions

--- next-component/src/stories/StyledButton.stories.tsx
+++ next-component/src/stories/StyledButton.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentMeta } from '@storybook/react';
 import StyledButton from '../components/StyledButton';
+import { action } from '@storybook/addon-actions';
+import { useState } from 'react';
 
 export default {
   title: 'StyledButton',
@@ -9,12 +11,20 @@ export default {
   },
 } as ComponentMeta<typeof StyledButton>;
 
+const incrementAction = action('increment');
+
 export const Primary = (
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
+  const [count, setCount] = useState(0);
+  const onClick = (e: React.MouseEvent) => {
+    incrementAction(e, count);
+    setCount((num) => num + 1);
+  };
+
   return (
-    <StyledButton {...props} variant='primary'>
-      Primary
+    <StyledButton {...props} variant='primary' onClick={onClick}>
+      Count: {count}
     </StyledButton>
   );
 };

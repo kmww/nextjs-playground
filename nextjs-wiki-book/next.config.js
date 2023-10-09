e2d7c01ea68c0f8972b7bdc959ad218b562e297e
kmw <kmw9708@gmail.com>
2023-10-09 16:02:25 +0900

    chore: jest 설정

--- nextjs-wiki-book/next.config.js
+++ nextjs-wiki-book/next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // production환경에서 react-testing 라이브러리에서 사용하는 data-testid 속성 삭제
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      };
+    }
+
+    return compilerConfig;
+  })(),
 };
 
 module.exports = nextConfig;

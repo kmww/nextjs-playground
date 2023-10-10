3c84335cda708f520868be1c238c7a29c50bbe0d
kmw <kmw9708@gmail.com>
2023-10-10 20:07:02 +0900

    feat: signin api

---
+++ nextjs-wiki-book/services/auth/signin.ts
@@ -0,0 +1,21 @@
+import { SigninParams } from '@/types/authTypes';
+import { fetcher } from '@/utils';
+
+const signin = async (
+  context: ApiContext,
+  params: SigninParams,
+): Promise<User> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, '')}/auth/signin`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    },
+  );
+};
+
+export default signin;

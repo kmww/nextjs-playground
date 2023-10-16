30604636a0ac316abe4c8e6745a976fe51a0072e
kmw <kmw9708@gmail.com>
2023-10-17 00:25:53 +0900

    feat: Box 컴포넌트 구현

--- nextjs-wiki-book/src/styles/themes/index.ts
+++ nextjs-wiki-book/src/styles/themes/index.ts
@@ -3,8 +3,9 @@ import fontSizes from './fontSizes';
 import letterSpacings from './letterSpacings';
 import lineHeights from './lineHeights';
 import space from './space';
+import { DefaultTheme } from 'styled-components';
 
-export const theme = {
+export const theme: DefaultTheme = {
   space,
   fontSizes,
   letterSpacings,

403a63dd00a917e6ca10525469a58c864fa2c692
kmw <kmw9708@gmail.com>
2023-10-16 15:08:11 +0900

    feat: product type

--- nextjs-wiki-book/src/types/data.d.ts
+++ nextjs-wiki-book/src/types/data.d.ts
@@ -1,2 +1,16 @@
+import { User } from './auth';
+
 export type Category = 'shoes' | 'clothes' | 'book';
 export type Condition = 'new' | 'used';
+
+export interface Product {
+  id: number;
+  category: Category;
+  title: string;
+  description: string;
+  imageUrl: string;
+  blurDataUrl: string;
+  price: number;
+  condition: Condition;
+  owner: User;
+}

e0a1645d6d470dd33dda4ed9817c4eb9599f131d
kmw <kmw9708@gmail.com>
2023-10-17 16:40:05 +0900

    feat: grid 타입설정

---
+++ nextjs-wiki-book/src/components/Grid/index.tsx
@@ -0,0 +1,23 @@
+import {
+  CSSPropertyGridArea,
+  CSSPropertyGridAutoFlow,
+  CSSPropertyGridColumn,
+  CSSPropertyGridRow,
+  Responsive,
+} from '@/types/styles';
+import { BoxProps } from '../layout/Box';
+
+type GridProps = BoxProps & {
+  gridGap?: Responsive<string>;
+  gridColumnGap?: Responsive<string>;
+  gridRowGap?: Responsive<string>;
+  gridColumn?: Responsive<CSSPropertyGridColumn>;
+  gridRow?: Responsive<CSSPropertyGridRow>;
+  gridAutoFlow?: Responsive<CSSPropertyGridAutoFlow>;
+  gridAutoColumns?: Responsive<string>;
+  gridAutoRows?: Responsive<string>;
+  gridTemplateColumns?: Responsive<string>;
+  gridTemplateRows?: Responsive<string>;
+  gridTemplateAreas?: Responsive<CSSPropertyGridArea>;
+  gridArea?: Responsive<string>;
+};

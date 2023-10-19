1d8063edf1a3ded093a39e5a90106723cd32da3b
kmw <kmw9708@gmail.com>
2023-10-20 01:01:44 +0900

    feat: atoms shapeImage 구현

--- nextjs-wiki-book/src/components/atoms/ShapeImage/index.tsx
+++ nextjs-wiki-book/src/components/atoms/ShapeImage/index.tsx
@@ -1,4 +1,17 @@
-import { ImageProps } from 'next/image';
+import Image, { ImageProps } from 'next/image';
+import styled from 'styled-components';
 
 type ImageShape = 'circle' | 'square';
 type ShapeImageProps = ImageProps & { shape?: ImageShape };
+
+const ImageWithShape = styled(Image)<{ shape?: ImageShape }>`
+  border-radius: ${({ shape }) => (shape === 'circle' ? '50%' : '0')};
+`;
+
+const ShapeImage = (props: ShapeImageProps) => {
+  const { shape, ...imageProps } = props;
+
+  return <ImageWithShape shape={shape} {...imageProps} />;
+};
+
+export default ShapeImage;

4f9c160a3374359f3bc91d99b6a06b5a79b04282
kmw <kmw9708@gmail.com>
2023-10-19 14:32:15 +0900

    feat: atoms 버튼 구현

--- nextjs-wiki-book/src/components/atoms/Button/index.tsx
+++ nextjs-wiki-book/src/components/atoms/Button/index.tsx
@@ -1,3 +1,4 @@
+import { theme } from '@/styles/themes';
 import { Responsive } from '@/types/styles';
 import {
   Color,
@@ -5,8 +6,10 @@ import {
   LetterSpacing,
   LineHeight,
   Space,
+  toPropValue,
 } from '@/utils/styles';
 import React from 'react';
+import styled from 'styled-components';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger';
 
@@ -87,3 +90,87 @@ const variants = {
     },
   },
 };
+
+const Button = styled.button<ButtonProps>`
+  ${({ variant, color, backgroundColor, pseudoClass, theme }) => {
+    if (variant && variants[variant]) {
+      const styles = [];
+      !color &&
+        styles.push(toPropValue('color', variants[variant].color, theme));
+      !backgroundColor &&
+        styles.push(
+          toPropValue(
+            'background-color',
+            variants[variant].backgroundColor,
+            theme,
+          ),
+        );
+      !pseudoClass &&
+        styles.push(
+          `&:hover {
+          ${toPropValue(
+            'background-color',
+            variants[variant].pseudoClass.hover.backgroundColor,
+            theme,
+          )}
+        }`.replaceAll('\n', ''),
+        );
+      !pseudoClass &&
+        styles.push(
+          `&:disabled {
+          ${toPropValue(
+            'background-color',
+            variants[variant].pseudoClass.disabled.backgroundColor,
+            theme,
+          )}
+        }`.replaceAll('\n', ''),
+        );
+      return styles.join('\n');
+    }
+  }}
+  ${(props) => toPropValue('font-size', props.fontSize, props.theme)}
+  ${(props) => toPropValue('letter-spacing', props.letterSpacing, props.theme)}
+  ${(props) => toPropValue('line-height', props.lineHeight, props.theme)}
+  ${(props) => toPropValue('color', props.color, props.theme)}
+  ${(props) =>
+    toPropValue('background-color', props.backgroundColor, props.theme)}
+  ${(props) => toPropValue('width', props.width, props.theme)}
+  ${(props) => toPropValue('height', props.height, props.theme)}
+  ${(props) => toPropValue('min-width', props.minHeight, props.theme)}
+  ${(props) => toPropValue('min-height', props.minHeight, props.theme)}
+  ${(props) => toPropValue('display', props.display, props.theme)}
+  ${(props) => toPropValue('border', props.border, props.theme)}
+  ${(props) => toPropValue('overflow', props.overflow, props.theme)}
+  ${(props) => toPropValue('margin', props.margin, props.theme)}
+  ${(props) => toPropValue('margin-top', props.marginTop, props.theme)}
+  ${(props) => toPropValue('margin-right', props.marginRight, props.theme)}
+  ${(props) => toPropValue('margin-bottom', props.marginBottom, props.theme)}
+  ${(props) => toPropValue('margin-left', props.marginLeft, props.theme)}
+  ${(props) => toPropValue('padding', props.padding, props.theme)}
+  ${(props) => toPropValue('padding-top', props.paddingTop, props.theme)}
+  ${(props) => toPropValue('padding-right', props.paddingRight, props.theme)}
+  ${(props) => toPropValue('padding-bottom', props.paddingBottom, props.theme)}
+  ${(props) => toPropValue('padding-left', props.paddingLeft, props.theme)}
+  &:hover {
+    ${(props) =>
+      toPropValue(
+        'background-color',
+        props.pseudoClass?.hover?.backgroundColor,
+      )}
+  }
+  &:disabled {
+    ${(props) =>
+      toPropValue(
+        'background-color',
+        props.pseudoClass?.disabled?.backgroundColor,
+      )}
+  }
+  cursor: pointer;
+  outline: 0;
+  text-decoration: 'none';
+  opacity: ${({ disabled }) => (disabled ? '0.5' : '1')};
+  border-radius: 4px;
+  border: none;
+`;
+
+export default Button;

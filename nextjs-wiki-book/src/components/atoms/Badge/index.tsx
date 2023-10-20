d5a792f60dcebac9bfded574ffb4edcaaaff3c69
kmw <kmw9708@gmail.com>
2023-10-20 18:24:23 +0900

    feat: atoms Badge 원형

---
+++ nextjs-wiki-book/src/components/atoms/Badge/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const BadgeWrapper = styled.div<{ backgroundColor: string }>`
+  border-radius: 20px;
+  height: 20px;
+  min-width: 20px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`;

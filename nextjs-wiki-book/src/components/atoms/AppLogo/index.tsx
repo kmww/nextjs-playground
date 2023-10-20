2c316cebd070b1a127ac924d630ee0304b30519a
kmw <kmw9708@gmail.com>
2023-10-21 04:08:33 +0900

    feat: AppLogo 구현

---
+++ nextjs-wiki-book/src/components/atoms/AppLogo/index.tsx
@@ -0,0 +1,34 @@
+const AppLogo = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="40" height="40" rx="6" fill="url(#paint0_linear)" />
+      <g
+        transform="translate(0.000000,40.000000) scale(0.100000,-0.100000)"
+        fill="white"
+      >
+        <path d="M240 310 c-8 -5 -48 -7 -87 -3 -62 5 -73 3 -79 -11 -8 -22 19 -61 53 -78 25 -12 28 -19 31 -68 4 -84 9 -97 44 -109 17 -6 29 -8 25 -5 -3 4 -1 16 5 28 6 11 11 37 9 57 -1 19 5 55 13 80 21 60 20 71 -6 67 -18 -2 -22 -10 -23 -43 -4 -168 -4 -165 -25 -165 -21 0 -21 -1 -24 175 -1 23 -6 31 -23 33 -18 3 -23 -2 -23 -20 l0 -23 -20 25 c-31 39 -26 49 23 43 23 -3 46 -9 51 -14 5 -5 9 -32 8 -61 -1 -29 2 -69 7 -88 7 -28 9 -17 10 58 1 100 -1 97 64 106 43 6 48 -7 16 -48 -14 -18 -16 -26 -7 -26 8 0 20 9 28 20 13 18 15 19 35 2 20 -17 20 -17 3 4 -10 11 -18 28 -18 38 0 9 -6 18 -12 20 -39 12 -65 14 -78 6z" />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear"
+          x1="0"
+          y1="20"
+          x2="40"
+          y2="20"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#0f0f0f" />
+          <stop offset="1" stopColor="#616161" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default AppLogo;

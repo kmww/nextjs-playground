106df9c941dd868dabfd2ef63b576a357098e3f3
kmw <kmw9708@gmail.com>
2023-10-20 17:51:11 +0900

    feat: atoms TextArea 구현

--- nextjs-wiki-book/src/components/atoms/TextArea/index.tsx
+++ nextjs-wiki-book/src/components/atoms/TextArea/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from 'react';
 import styled from 'styled-components';
 
 export interface TextAreaProps
@@ -27,3 +28,66 @@ const StyledTextArea = styled.textarea<{ hasError?: boolean }>`
     color: ${({ theme }) => theme.colors.placeholder};
   }
 `;
+
+const TextArea = (props: TextAreaProps) => {
+  const {
+    rows = 5,
+    minRows = 5,
+    maxRows = 10,
+    children,
+    hasError,
+    onChange,
+    ...rest
+  } = props;
+  const [textareaRows, setTextareaRows] = useState(Math.min(rows, minRows));
+
+  console.assert(
+    !(rows < minRows),
+    'TextArea: rows should be greater than minRows',
+  );
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const textareaLineHeight = 24;
+      const previousRows = e.target.rows;
+
+      e.target.rows = minRows;
+      const currentRows = Math.floor(
+        e.target.scrollHeight / textareaLineHeight,
+      );
+
+      if (currentRows === previousRows) {
+        e.target.rows = currentRows;
+      }
+
+      if (currentRows >= maxRows) {
+        e.target.rows = maxRows;
+        e.target.scrollTop = e.target.scrollHeight;
+      }
+
+      setTextareaRows(currentRows < maxRows ? currentRows : maxRows);
+      onChange && onChange(e);
+    },
+    [onChange, minRows, maxRows],
+  );
+
+  return (
+    <StyledTextArea
+      hasError={hasError}
+      onChange={handleChange}
+      aria-label={rest.placeholder}
+      rows={textareaRows}
+      {...rest}
+    >
+      {children}
+    </StyledTextArea>
+  );
+};
+
+TextArea.defaultProps = {
+  rows: 5,
+  minRows: 5,
+  maxRows: 10,
+};
+
+export default TextArea;

dc2988b7ac49047a7e964e6d1325ccfa69ec5042
kmw <kmw9708@gmail.com>
2023-10-20 17:43:17 +0900

    feat: atoms StyledTextarea

--- nextjs-wiki-book/src/components/atoms/TextArea/index.tsx
+++ nextjs-wiki-book/src/components/atoms/TextArea/index.tsx
@@ -1,6 +1,29 @@
+import styled from 'styled-components';
+
 export interface TextAreaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   minRows?: number;
   maxRows?: number;
   hasError?: boolean;
 }
+
+const StyledTextArea = styled.textarea<{ hasError?: boolean }>`
+  color: ${({ theme }) => theme.colors.inputText};
+  border: 1px solid
+    ${({ theme, hasError }) =>
+      hasError ? theme.colors.danger : theme.colors.border};
+  border-radius: 5px;
+  box-sizing: border-box;
+  outline: none;
+  width: 100%;
+  font-size: 16px;
+  line-height: 24px;
+  padding: 9px 12px 10px 12px;
+  resize: none;
+  overflow: auto;
+  height: auto;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.placeholder};
+  }
+`;

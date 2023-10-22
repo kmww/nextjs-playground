f9d31f00b512f7ebeec5810edb16abd9d5fa0afd
kmw <kmw9708@gmail.com>
2023-10-22 21:15:32 +0900

    feat: BreadcrumbItem storybook

--- nextjs-wiki-book/src/components/atoms/BreadcrumbItem/indext.stories.tsx
+++ nextjs-wiki-book/src/components/atoms/BreadcrumbItem/indext.stories.tsx
@@ -6,3 +6,13 @@ const meta: Meta<typeof BreadcrumbItem> = {
 };
 
 export default meta;
+
+export const Standard = {
+  render: () => (
+    <div>
+      <BreadcrumbItem>Item 1</BreadcrumbItem>
+      <BreadcrumbItem>Item 2</BreadcrumbItem>
+      <BreadcrumbItem>Item 3</BreadcrumbItem>
+    </div>
+  ),
+};

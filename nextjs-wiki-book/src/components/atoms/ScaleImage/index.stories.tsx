15d9632f5a76600e9551847e7c9d6ae07fd3e96f
kmw <kmw9708@gmail.com>
2023-10-22 21:36:04 +0900

    feat: ScaleImage meta

---
+++ nextjs-wiki-book/src/components/atoms/ScaleImage/index.stories.tsx
@@ -0,0 +1,49 @@
+import { Meta } from '@storybook/react';
+import ScaleImage from './';
+
+const meta: Meta<typeof ScaleImage> = {
+  title: 'Atoms/ScaleImage',
+  argTypes: {
+    src: {
+      control: { type: 'text' },
+      description: '이미지 URL',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    width: {
+      control: { type: 'number' },
+      description: '이미지 너비',
+      defaultValue: 320,
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    height: {
+      control: { type: 'number' },
+      description: '이미지 높이',
+      defaultValue: 320,
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    containerWidth: {
+      control: { type: 'number' },
+      defaultValue: 320,
+      description: '너비',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    containerHeight: {
+      control: { type: 'number' },
+      defaultValue: 320,
+      description: '높이',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+  },
+};
+
+export default meta;

7d45e7d2cd6857a21e8a89b45d15a7712e4a0296
kmw <kmw9708@gmail.com>
2023-10-22 21:12:49 +0900

    feat: BreadcrumbItem meta

---
+++ nextjs-wiki-book/src/components/atoms/BreadcrumbItem/indext.stories.tsx
@@ -0,0 +1,8 @@
+import { Meta } from '@storybook/react';
+import BreadcrumbItem from './';
+
+const meta: Meta<typeof BreadcrumbItem> = {
+  title: 'Atoms/BreadcrumbItem',
+};
+
+export default meta;

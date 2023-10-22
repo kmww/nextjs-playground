3a14d557a83119ad4283c1fba9d79717494b7ca0
kmw <kmw9708@gmail.com>
2023-10-22 21:20:58 +0900

    feat: atoms ScaleImage Container 구현

--- nextjs-wiki-book/src/components/atoms/ScaleImage/index.tsx
+++ nextjs-wiki-book/src/components/atoms/ScaleImage/index.tsx
@@ -1,7 +1,18 @@
 import { Responsive } from '@/types/styles';
+import { toPropValue } from '@/utils/styles';
 import { ImageProps } from 'next/image';
+import styled from 'styled-components';
 
 type ScaleImageProps = Omit<ImageProps, 'quality'> & {
   containerWidth?: Responsive<string>;
   containerHeight?: Responsive<string>;
 };
+
+const ScaleEffectImageContainer = styled.div<{
+  width: Responsive<string>;
+  height: Responsive<string>;
+}>`
+  overflow: hidden;
+  ${({ width, theme }) => toPropValue('width', width, theme)}
+  ${({ height, theme }) => toPropValue('height', height, theme)}
+`;

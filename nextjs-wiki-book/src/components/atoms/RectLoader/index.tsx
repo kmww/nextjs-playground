4dc78274d7812067d6e9094d51de6b7bc0b9c30a
kmw <kmw9708@gmail.com>
2023-10-23 19:08:40 +0900

    fix: atoms RectLoader import 수정 및 오류수정

--- nextjs-wiki-book/src/components/atoms/RectLoader/index.tsx
+++ nextjs-wiki-book/src/components/atoms/RectLoader/index.tsx
@@ -1,4 +1,5 @@
-import ContentLoader, { IContentLoaderProps } from 'react-content-loader';
+import ContentLoader from 'react-content-loader';
+import type { IContentLoaderProps } from 'react-content-loader';
 
 interface RectLoaderProps extends IContentLoaderProps {
   width: number;
@@ -6,17 +7,19 @@ interface RectLoaderProps extends IContentLoaderProps {
 }
 
 const RectLoader = ({ width, height, ...rest }: RectLoaderProps) => {
-  <ContentLoader
-    speed={2}
-    width={width}
-    height={height}
-    viewBox={`0 0 ${width} ${height}`}
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-    {...rest}
-  >
-    <rect x="0" y="0" rx="0" ry="0" width={width} height={height} />
-  </ContentLoader>;
+  return (
+    <ContentLoader
+      speed={2}
+      width={width}
+      height={height}
+      viewBox={`0 0 ${width} ${height}`}
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...rest}
+    >
+      <rect x="0" y="0" rx="0" ry="0" width={width} height={height} />
+    </ContentLoader>
+  );
 };
 
 export default RectLoader;

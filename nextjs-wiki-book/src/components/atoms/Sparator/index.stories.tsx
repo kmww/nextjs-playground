91de0cad19b6c5c39ff9231bd92c69e880dd813a
kmw <kmw9708@gmail.com>
2023-10-23 18:54:45 +0900

    feat: Separator storybook

--- nextjs-wiki-book/src/components/atoms/Sparator/index.stories.tsx
+++ nextjs-wiki-book/src/components/atoms/Sparator/index.stories.tsx
@@ -1,8 +1,20 @@
-import { Meta } from '@storybook/react';
-import Separator from '.';
+import { Meta, StoryObj } from '@storybook/react';
+import Separator from './';
 
 const meta: Meta<typeof Separator> = {
   title: 'Atoms/Separator',
 };
 
 export default meta;
+
+type Story = StoryObj<typeof Separator>;
+
+export const Standard: Story = {
+  render: () => (
+    <>
+      <Separator>or</Separator>
+      <Separator>and</Separator>
+      <Separator />
+    </>
+  ),
+};

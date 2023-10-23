ab4f3d4a9819bb3ae6c0c8b3b6398a2aae1fdaa8
kmw <kmw9708@gmail.com>
2023-10-23 18:40:55 +0900

    feat: atoms Spinner 구현

--- nextjs-wiki-book/src/components/atoms/Spinner/index.tsx
+++ nextjs-wiki-book/src/components/atoms/Spinner/index.tsx
@@ -44,3 +44,26 @@ type SpinnerProps = {
   strokeWidth?: number;
   isAutoCentering?: boolean;
 };
+
+const Spinner = (props: SpinnerProps) => {
+  const { size = 50, strokeWidth = 4, isAutoCentering = false } = props;
+
+  return (
+    <StyledSpinner
+      size={size}
+      isAutoCentering={isAutoCentering}
+      viewBox={`0 0 ${size} ${size}`}
+    >
+      <circle
+        className="path"
+        cx={size / 2}
+        cy={size / 2}
+        r={size / 2 - strokeWidth / 2}
+        fill="none"
+        strokeWidth={strokeWidth}
+      />
+    </StyledSpinner>
+  );
+};
+
+export default Spinner;

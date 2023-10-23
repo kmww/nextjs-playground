fd8428e3e5b7b692fb3c285573dcc7f7a4f83a31
kmw <kmw9708@gmail.com>
2023-10-23 18:43:50 +0900

    feat: Spinner meta

---
+++ nextjs-wiki-book/src/components/atoms/Spinner/index.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta } from '@storybook/react';
+import Spinner from './';
+
+const meta: Meta<typeof Spinner> = {
+  title: 'Atoms/Spinner',
+  argTypes: {
+    size: {
+      control: { type: 'number' },
+      defaultValue: 50,
+      description: '크기',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    isAutoCentering: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+      description: '센터링 플래그',
+      table: {
+        type: { summary: 'boolean' },
+      },
+    },
+    strokeWidth: {
+      control: { type: 'number' },
+      defaultValue: 4,
+      description: '선 폭',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+  },
+};
+
+export default meta;

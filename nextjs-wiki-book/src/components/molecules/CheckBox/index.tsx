09b5fb53b44a4d70e29cb2da2016352145394d85
kmw <kmw9708@gmail.com>
2023-10-24 18:20:29 +0900

    feat: molecules CheckBox style

--- nextjs-wiki-book/src/components/molecules/CheckBox/index.tsx
+++ nextjs-wiki-book/src/components/molecules/CheckBox/index.tsx
@@ -1,4 +1,16 @@
+import styled from 'styled-components';
+
 export interface CheckBoxProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'defaultValue'> {
   label?: string;
 }
+
+const CheckBoxElement = styled.input`
+  display: none;
+`;
+
+const Label = styled.label`
+  cursor: pointer;
+  margin-left: 6px;
+  user-select: none;
+`;

75bcebd3a409f6e7e3e940f6273919bc2e323b50
kmw <kmw9708@gmail.com>
2023-10-24 18:37:18 +0900

    feat: CheckBox meta

---
+++ nextjs-wiki-book/src/components/molecules/CheckBox/index.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta } from '@storybook/react';
+import CheckBox from './';
+
+const meta: Meta<typeof CheckBox> = {
+  title: 'Molecules/CheckBox',
+  argTypes: {
+    label: {
+      control: { type: 'text' },
+      description: '체크박스 라벨',
+      table: {
+        type: { summary: 'text' },
+      },
+    },
+    checked: {
+      control: { type: 'boolean' },
+      description: '체크',
+      table: {
+        type: { summary: 'boolean' },
+      },
+    },
+    onChange: {
+      description: '체크 변경시 이벤트 핸들러',
+      table: {
+        type: { summary: 'function' },
+      },
+    },
+  },
+};
+
+export default meta;

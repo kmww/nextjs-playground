9080fb3a7bbca760f8b5b71e5fb88d6a086461a2
kmw <kmw9708@gmail.com>
2023-10-25 16:09:27 +0900

    feat: molecules Dropzone getFile

--- nextjs-wiki-book/src/components/molecules/Dropzone/index.tsx
+++ nextjs-wiki-book/src/components/molecules/Dropzone/index.tsx
@@ -5,3 +5,13 @@ const isDragEvent = (value: any): value is React.DragEvent => {
 const isInput = (value: EventTarget | null): value is HTMLInputElement => {
   return value !== null;
 };
+
+const getFilesFromEvent = (e: React.DragEvent | React.ChangeEvent): File[] => {
+  if (isDragEvent(e)) {
+    return Array.from(e.dataTransfer.files);
+  } else if (isInput(e.target) && e.target.files) {
+    return Array.from(e.target.files);
+  }
+
+  return [];
+};

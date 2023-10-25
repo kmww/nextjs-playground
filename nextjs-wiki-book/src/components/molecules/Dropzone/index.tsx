ed54eab1ace113187e43e3253b5ed1f792866230
kmw <kmw9708@gmail.com>
2023-10-25 16:07:49 +0900

    feat: molecules Dropzone check input el

--- nextjs-wiki-book/src/components/molecules/Dropzone/index.tsx
+++ nextjs-wiki-book/src/components/molecules/Dropzone/index.tsx
@@ -1,3 +1,7 @@
 const isDragEvent = (value: any): value is React.DragEvent => {
   return !!value.dataTransfer;
 };
+
+const isInput = (value: EventTarget | null): value is HTMLInputElement => {
+  return value !== null;
+};

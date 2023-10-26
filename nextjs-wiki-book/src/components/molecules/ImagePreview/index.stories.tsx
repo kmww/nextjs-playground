9034d272119ee811a87806652ef661c90fc8a986
kmw <kmw9708@gmail.com>
2023-10-26 17:05:57 +0900

    feat: ImagePreview storybook

--- nextjs-wiki-book/src/components/molecules/ImagePreview/index.stories.tsx
+++ nextjs-wiki-book/src/components/molecules/ImagePreview/index.stories.tsx
@@ -1,6 +1,8 @@
-import { Meta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import ImagePreview from './';
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import Dropzone from '../Dropzone';
 
 const meta: Meta<typeof ImagePreview> = {
   title: 'Molecules/ImagePreview',
@@ -55,3 +57,57 @@ interface Image {
   file?: File;
   src?: string;
 }
+
+type Story = StoryObj<typeof ImagePreview>;
+
+const Template: Story = {
+  render: (args) => {
+    const [files, setFiles] = useState<File[]>([]);
+    const [images, setImages] = useState<Image[]>([]);
+
+    useEffect(() => {
+      const newImages = [...images];
+
+      for (const file of files) {
+        const index = newImages.findIndex((img: Image) => img.file === file);
+        if (index === -1) {
+          newImages.push({
+            file,
+            src: URL.createObjectURL(file),
+          });
+        }
+      }
+      setImages(newImages);
+    }, [files]);
+
+    const handleRemove = (src: string) => {
+      const image = images.find((img: Image) => img.src === src);
+
+      if (image !== undefined) {
+        setImages((images) => images.filter((img) => img.src !== image.src));
+        setFiles((files) => files.filter((file: File) => file !== image.file));
+      }
+
+      args && args.onRemove && args.onRemove(src);
+    };
+
+    return (
+      <Container>
+        <Dropzone value={files} onDrop={(fileList) => setFiles(fileList)} />
+        {images.map((image, index) => (
+          <ImagePreview
+            key={index}
+            src={image.src}
+            width="100px"
+            {...args}
+            onRemove={handleRemove}
+          />
+        ))}
+      </Container>
+    );
+  },
+};
+
+export const WithDropzone = {
+  ...Template,
+};

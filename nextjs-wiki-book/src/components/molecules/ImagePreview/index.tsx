67b52b0ab32b9bbab64fbd5292b9f216bbfd4d46
kmw <kmw9708@gmail.com>
2023-10-26 16:48:09 +0900

    feat: molecules ImagePreview layout

---
+++ nextjs-wiki-book/src/components/molecules/ImagePreview/index.tsx
@@ -0,0 +1,18 @@
+import Box from '@/components/layout/Box';
+import Flex from '@/components/layout/Flex';
+import styled from 'styled-components';
+
+const ImagePreviewContainer = styled(Box)`
+  position: relative;
+`;
+
+const CloseBox = styled(Flex)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 30px;
+  height: 30px;
+  border-radius: 0 6px 0 6px;
+  background-color: rgba(44, 44, 44, 0.66);
+  cursor: pointer;
+`;

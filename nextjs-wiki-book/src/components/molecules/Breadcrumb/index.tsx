d88d5c69dabf01a878c574d0d4baac6abceca89d
kmw <kmw9708@gmail.com>
2023-10-27 18:40:51 +0900

    feat: molecules Breadcrumb root

---
+++ nextjs-wiki-book/src/components/molecules/Breadcrumb/index.tsx
@@ -0,0 +1,8 @@
+import Flex from '@/components/layout/Flex';
+import styled from 'styled-components';
+
+const BreadcrumbRoot = styled(Flex)`
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+`;

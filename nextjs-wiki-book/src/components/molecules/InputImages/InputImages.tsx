24a461fc6146c33373da4d3daf842e71f52d264a
kmw <kmw9708@gmail.com>
2023-10-27 20:43:41 +0900

    feat: molecules InputImages 상태&핸들러

---
+++ nextjs-wiki-book/src/components/molecules/InputImages/InputImages.tsx
@@ -0,0 +1,14 @@
+import { InputImagesProps } from './indext';
+
+const InputImages = (props: InputImagesProps) => {
+  const {
+    images,
+    maximumNumber,
+    name,
+    hasError,
+    width = '100%',
+    height = '260px',
+    onChange,
+  } = props;
+  const files = useMemo;
+};
--- nextjs-wiki-book/src/components/molecules/InputImages/indext.tsx
+++ nextjs-wiki-book/src/components/molecules/InputImages/indext.tsx
@@ -1,4 +1,5 @@
 import Flex from '@/components/layout/Flex';
+import { useCallback, useMemo } from 'react';
 import styled from 'styled-components';
 
 const InputImagesContainer = styled(Flex)`
@@ -24,3 +25,56 @@ interface InputImagesProps {
   height?: string;
   onChange: (imges: FileData[]) => void;
 }
+
+const InputImages = (props: InputImagesProps) => {
+  const {
+    images,
+    maximumNumber,
+    name,
+    hasError,
+    width = '100%',
+    height = '260px',
+    onChange,
+  } = props;
+  const files = useMemo(() => {
+    images.filter((img: FileData) => img.file).map((img: FileData) => img.file);
+  }, [images]);
+
+  const isDropzoneDisplay =
+    !maximumNumber || images.length < maximumNumber ? 'block' : 'none';
+
+  const onRemove = useCallback(
+    (src: string) => {
+      const image = images.find((img: FileData) => img.src === src);
+      const newImages = images.filter((imgs: FileData) => imgs.src !== src);
+
+      if (image) {
+        if (image.file && image.src) {
+          URL.revokeObjectURL(image.src);
+          delete image.src;
+        }
+        onChange && onChange(newImages);
+      }
+    },
+    [images, onChange],
+  );
+
+  const onDrop = useCallback(
+    (files: File[]) => {
+      const newImages = [];
+
+      for (const file of files) {
+        const img = images.find((img: FileData) => img.file === file);
+
+        if (
+          !img &&
+          (!maximumNumber || images.length + newImages.length < maximumNumber)
+        ) {
+          newImages.push({ file, src: URL.createObjectURL(file) });
+        }
+      }
+      onChange && onChange(newImages);
+    },
+    [images, maximumNumber, onChange],
+  );
+};

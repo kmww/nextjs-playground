3832da72eba489498c919048648976986b831972
kmw <kmw9708@gmail.com>
2023-10-27 20:57:09 +0900

    feat: InputImages storybook

--- nextjs-wiki-book/src/components/molecules/InputImages/index.stories.tsx
+++ nextjs-wiki-book/src/components/molecules/InputImages/index.stories.tsx
@@ -1,8 +1,31 @@
 import { Meta } from '@storybook/react';
-import InputImages from './';
+import InputImages, { FileData } from './';
+import styled from 'styled-components';
+import { useState } from 'react';
 
 const meta: Meta<typeof InputImages> = {
   title: 'Molecules/InputImages',
 };
 
 export default meta;
+
+const Container = styled.div`
+  width: 288px;
+  display: grid;
+  gap: 10px;
+  grid-template-columns: 1fr;
+`;
+
+export const Standard = () => {
+  const [images, setImages] = useState<FileData[]>([]);
+
+  const handleChange = (images: FileData[]) => {
+    setImages(images);
+  };
+
+  return (
+    <Container>
+      <InputImages images={images} onChange={handleChange} maximumNumber={2} />
+    </Container>
+  );
+};

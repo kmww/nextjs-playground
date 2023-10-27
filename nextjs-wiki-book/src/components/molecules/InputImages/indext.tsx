e110c51feae67c81d6048efce08f1a0d21a817b2
kmw <kmw9708@gmail.com>
2023-10-27 20:35:17 +0900

    feat: molecules InputImages types

--- nextjs-wiki-book/src/components/molecules/InputImages/indext.tsx
+++ nextjs-wiki-book/src/components/molecules/InputImages/indext.tsx
@@ -6,3 +6,21 @@ const InputImagesContainer = styled(Flex)`
     margin-top: 8px;
   }
 `;
+
+export type FileData = {
+  id?: string;
+  src?: string;
+  file?: File;
+  selected?: boolean;
+  chosen?: boolean;
+};
+
+interface InputImagesProps {
+  name?: string;
+  images: FileData[];
+  maximumNumber?: number;
+  hasError?: boolean;
+  width?: string;
+  height?: string;
+  onChange: (imges: FileData[]) => void;
+}

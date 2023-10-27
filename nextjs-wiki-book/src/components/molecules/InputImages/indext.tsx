5f8468caa95f3ea14bc3cee92bd21235113f0066
kmw <kmw9708@gmail.com>
2023-10-27 20:33:38 +0900

    feat: molecules InputImages container

---
+++ nextjs-wiki-book/src/components/molecules/InputImages/indext.tsx
@@ -0,0 +1,8 @@
+import Flex from '@/components/layout/Flex';
+import styled from 'styled-components';
+
+const InputImagesContainer = styled(Flex)`
+  & > *:not(:first-child) {
+    margin-top: 8px;
+  }
+`;

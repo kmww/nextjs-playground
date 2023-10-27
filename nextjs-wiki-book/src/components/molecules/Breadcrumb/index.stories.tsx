c8f1286454c3eef919e803422ec5340a4b1e5b0e
kmw <kmw9708@gmail.com>
2023-10-27 18:46:03 +0900

    feat: Breadcrumb storybook

--- nextjs-wiki-book/src/components/molecules/Breadcrumb/index.stories.tsx
+++ nextjs-wiki-book/src/components/molecules/Breadcrumb/index.stories.tsx
@@ -1,8 +1,21 @@
 import { Meta } from '@storybook/react';
 import Breadcrumb from './';
+import BreadcrumbItem from '@/components/atoms/BreadcrumbItem';
 
 const meta: Meta<typeof Breadcrumb> = {
   title: 'Molecules/Breadcrumb',
 };
 
 export default meta;
+
+export const Standard = () => (
+  <Breadcrumb>
+    <BreadcrumbItem>
+      <a href="#">Top</a>
+    </BreadcrumbItem>
+    <BreadcrumbItem>
+      <a href="#">Clothes</a>
+    </BreadcrumbItem>
+    <BreadcrumbItem>Item</BreadcrumbItem>
+  </Breadcrumb>
+);

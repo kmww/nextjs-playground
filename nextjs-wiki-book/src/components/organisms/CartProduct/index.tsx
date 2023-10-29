9e55e82b9ce6d30c46c56a8087e180cff21de286
kmw <kmw9708@gmail.com>
2023-10-29 20:39:22 +0900

    feat: CartProduct RemoveText

---
+++ nextjs-wiki-book/src/components/organisms/CartProduct/index.tsx
@@ -0,0 +1,9 @@
+import Text from '@/components/atoms/Text';
+import styled from 'styled-components';
+
+const RemoveText = styled(Text)`
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;

b750808bdaa5957abbf1eafc40615eb46896a9bd
kmw <kmw9708@gmail.com>
2023-10-30 23:18:49 +0900

    feat: GlobalSpinner meta

---
+++ nextjs-wiki-book/src/components/organisms/GlobalSpinner/index.stories.tsx
@@ -0,0 +1,8 @@
+import { Meta } from '@storybook/react';
+import GlobalSpinner from './';
+
+const meta: Meta<typeof GlobalSpinner> = {
+  title: 'Organisms/GlobalSpinner',
+};
+
+export default meta;

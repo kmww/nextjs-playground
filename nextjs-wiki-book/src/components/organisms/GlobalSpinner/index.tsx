db58774424020fc3007c8a85446fd2d5e453f404
kmw <kmw9708@gmail.com>
2023-10-30 23:07:22 +0900

    feat: GlobalSpinner Wrapper

---
+++ nextjs-wiki-book/src/components/organisms/GlobalSpinner/index.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const GlobalSpinnerWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(255, 255, 255 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1200;
+`;

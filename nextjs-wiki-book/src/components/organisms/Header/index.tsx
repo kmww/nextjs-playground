9d7f7dd881c7ca5662110aa174c901c05610cace
kmw <kmw9708@gmail.com>
2023-10-31 18:30:34 +0900

    feat: Header Nav

--- nextjs-wiki-book/src/components/organisms/Header/index.tsx
+++ nextjs-wiki-book/src/components/organisms/Header/index.tsx
@@ -1,7 +1,18 @@
+import Flex from '@/components/layout/Flex';
 import styled from 'styled-components';
 
 const HeaderRoot = styled.header`
   height: 88px;
   padding: ${({ theme }) => theme.space[2]} 0px;
   border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+`;
+
+const Nav = styled(Flex)`
+  & > span:not(:first-child) {
+    margin-left: ${({ theme }) => theme.space[2]};
+  }
+`;
+
+const NavLink = styled.span`
+  display: inline;
 `;

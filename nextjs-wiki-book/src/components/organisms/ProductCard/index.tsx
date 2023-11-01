ea26f5694ad8167f72f38ae28fb5b77c64820612
kmw <kmw9708@gmail.com>
2023-11-01 22:12:42 +0900

    feat: ProductCard styledComp

--- nextjs-wiki-book/src/components/organisms/ProductCard/index.tsx
+++ nextjs-wiki-book/src/components/organisms/ProductCard/index.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 interface ProductCardProps {
   title: string;
   price: number;
@@ -5,3 +7,18 @@ interface ProductCardProps {
   blurDataUrl?: string;
   variant?: 'listing' | 'small' | 'detail';
 }
+
+const ProductCardContainer = styled.div`
+  position: relative;
+`;
+
+const ProductCardImageContainer = styled.div`
+  z-index: 99;
+`;
+
+const ProductCardInfo = styled.div`
+  position: absolute;
+  z-index: 100;
+  top: 0px;
+  left: 0px;
+`;

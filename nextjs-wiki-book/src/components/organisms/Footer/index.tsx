405c00e43dc3f23a736dc0579b8636bde62a7e08
kmw <kmw9708@gmail.com>
2023-11-09 18:01:47 +0900

    feat: Footer

--- nextjs-wiki-book/src/components/organisms/Footer/index.tsx
+++ nextjs-wiki-book/src/components/organisms/Footer/index.tsx
@@ -1,4 +1,8 @@
+import { GitHubIcon } from '@/components/atoms/IconButton';
 import Text from '@/components/atoms/Text';
+import Box from '@/components/layout/Box';
+import Flex from '@/components/layout/Flex';
+import Link from 'next/link';
 import styled from 'styled-components';
 
 const Anchor = styled(Text)`
@@ -7,3 +11,72 @@ const Anchor = styled(Text)`
     text-decoration: underline;
   }
 `;
+
+const Footer = () => {
+  return (
+    <footer>
+      <Flex flexDirection={{ base: 'column', md: 'row' }}>
+        <Box
+          minWidth={{ base: '100%', md: '120px' }}
+          paddingRight={{ base: 0, md: 1 }}
+        >
+          <nav>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor>홈</Anchor>
+              </Link>
+            </Box>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor>채용</Anchor>
+              </Link>
+            </Box>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor>알림</Anchor>
+              </Link>
+            </Box>
+          </nav>
+        </Box>
+        <Box
+          minWidth={{ base: '100%', md: '120px' }}
+          paddingRight={{ base: 0, md: 1 }}
+        >
+          <nav>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor>사용 규약</Anchor>
+              </Link>
+            </Box>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor>개인 정보 정책</Anchor>
+              </Link>
+            </Box>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor>배송 및 반품</Anchor>
+              </Link>
+            </Box>
+          </nav>
+        </Box>
+        <Box minWidth={{ base: '100%', md: '120px' }}>
+          <nav>
+            <Anchor
+              as="a"
+              href="https://github.com/kmww/nextjs-playground/tree/main/nextjs-wiki-book"
+              target="_blank"
+            >
+              <GitHubIcon size={22} />
+            </Anchor>
+          </nav>
+        </Box>
+      </Flex>
+      <Box paddingTop={3} paddingBottom={2}>
+        <Text>© 2023 Cojet Co., Ltd.. All rights reserved.</Text>
+      </Box>
+    </footer>
+  );
+};
+
+export default Footer;

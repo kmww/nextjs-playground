9f2eb6ea4d7c709a796d815c49dd76afe31b7f3d
kmw <kmw9708@gmail.com>
2023-11-09 17:52:24 +0900

    feat: Footer Anchor

---
+++ nextjs-wiki-book/src/components/organisms/Footer/index.tsx
@@ -0,0 +1,9 @@
+import Text from '@/components/atoms/Text';
+import styled from 'styled-components';
+
+const Anchor = styled(Text)`
+  cursor: potiner;
+  &:hover {
+    text-decoration: underline;
+  }
+`;

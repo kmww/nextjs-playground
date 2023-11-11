2743afa4a03716265cfa308b1f801536a9634c78
kmw <kmw9708@gmail.com>
2023-11-11 14:44:12 +0900

    feat: 로그인 이벤트 핸들러

--- nextjs-wiki-book/src/components/containers/SigninFormContainer.tsx
+++ nextjs-wiki-book/src/components/containers/SigninFormContainer.tsx
@@ -1,3 +1,28 @@
+import { useAuthContext } from '@/contexts/AuthContext';
+import { useGlobalSpinnerActionsContext } from '@/contexts/GlobalSpinnerContext';
+
 interface SigninFormContainerProps {
   onSingin: (error?: Error) => void;
 }
+
+const SigninFormContainer = ({ onSingin }: SigninFormContainerProps) => {
+  const { signin } = useAuthContext();
+  const setGlobalSpinner = useGlobalSpinnerActionsContext();
+
+  const handleSignin = async (username: string, password: string) => {
+    try {
+      setGlobalSpinner(true);
+      await signin(username, password);
+      onSingin && onSingin();
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        window.alert(err.message);
+        onSingin && onSingin(err);
+      }
+    } finally {
+      setGlobalSpinner(false);
+    }
+  };
+};
+
+export default SigninFormContainer;

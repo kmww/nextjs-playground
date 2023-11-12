33373a11b38f0458e5691103e62dfd1a5adc8696
kmw <kmw9708@gmail.com>
2023-11-12 19:06:59 +0900

    feat: user-user hook

--- nextjs-wiki-book/src/services/users/use-user.ts
+++ nextjs-wiki-book/src/services/users/use-user.ts
@@ -1,4 +1,5 @@
-import { User } from '@/types';
+import useSWR from 'swr';
+import { ApiContext, User } from '@/types';
 
 export interface UseUserProps {
   id: number;
@@ -10,3 +11,20 @@ export interface UseUser {
   isLoading: boolean;
   isError: boolean;
 }
+
+const useUser = (
+  context: ApiContext,
+  { id, initial }: UseUserProps,
+): UseUser => {
+  const { data, error } = useSWR<User>(
+    `${context.apiRootUrl.replace(/\/$/g, '')}/users/${id}`,
+  );
+
+  return {
+    user: data ?? initial,
+    isLoading: !error && !data,
+    isError: !!error,
+  };
+};
+
+export default useUser;

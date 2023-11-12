8f8ec4be4e155123db17154f8432c2fd7ab1f3cd
kmw <kmw9708@gmail.com>
2023-11-12 19:11:21 +0900

    feat: UserProfile Container

--- nextjs-wiki-book/src/components/containers/UserProfileContainer.tsx
+++ nextjs-wiki-book/src/components/containers/UserProfileContainer.tsx
@@ -1,4 +1,6 @@
+import useUser from '@/services/users/use-user';
 import { ApiContext, User } from '@/types';
+import UserProfile from '@/components/organisms/UserProfile';
 
 const context: ApiContext = {
   apiRootUrl: process.env.NEXT_PUBLIC_API_BASE_PATH || '/api/proxy',
@@ -8,3 +10,20 @@ interface UserProfileContainerProps {
   userId: number;
   user?: User;
 }
+
+const UserProfileContainer = ({ userId, user }: UserProfileContainerProps) => {
+  const { user: u } = useUser(context, { id: userId, initial: user });
+
+  if (!u) return <div>Loading</div>;
+
+  return (
+    <UserProfile
+      username={`${u.username} (${u.displayName})`}
+      profileImageUrl={u.profileImageUrl}
+      numberOfProducts={100}
+      description={u.description}
+    />
+  );
+};
+
+export default UserProfileContainer;

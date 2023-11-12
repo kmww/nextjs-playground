06fa57d737aca7613751f9226396d8ad471de610
kmw <kmw9708@gmail.com>
2023-11-12 19:24:11 +0900

    feat: use-search hook

--- nextjs-wiki-book/src/services/products/use-search.ts
+++ nextjs-wiki-book/src/services/products/use-search.ts
@@ -1,4 +1,5 @@
-import { Category, Condition, Product } from '@/types';
+import { ApiContext, Category, Condition, Product } from '@/types';
+import useSWR from 'swr';
 
 export interface UseSearchProps {
   category?: Category;
@@ -14,3 +15,37 @@ export interface UseSearch {
   isLoading: boolean;
   isError: boolean;
 }
+
+const useSearch = (
+  context: ApiContext,
+  {
+    category,
+    conditions,
+    userId,
+    sort = 'id',
+    order = 'desc',
+    initial,
+  }: UseSearchProps = {},
+): UseSearch => {
+  const path = `${context.apiRootUrl.replace(/\/$/g, '')}/products`;
+  const params = new URLSearchParams();
+
+  category && params.append('category', category);
+  userId && params.append('owner.id', `${userId}`);
+  conditions &&
+    conditions.forEach((condition) => params.append('condition', condition));
+  sort && params.append('_sort', sort);
+  order && params.append('_order', order);
+  const query = params.toString();
+  const { data, error } = useSWR<Product[]>(
+    query.length > 0 ? `${path}?${query}` : path,
+  );
+
+  return {
+    products: data ?? initial ?? [],
+    isLoading: !error && !data,
+    isError: !!error,
+  };
+};
+
+export default useSearch;

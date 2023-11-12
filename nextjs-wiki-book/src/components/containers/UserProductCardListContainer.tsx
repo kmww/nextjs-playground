a86d0e3f46e14abb3bd053aa7b9b451181ded753
kmw <kmw9708@gmail.com>
2023-11-12 19:27:17 +0900

    feat: UserProductCardListContainer

--- nextjs-wiki-book/src/components/containers/UserProductCardListContainer.tsx
+++ nextjs-wiki-book/src/components/containers/UserProductCardListContainer.tsx
@@ -1,4 +1,9 @@
+import useSearch from '@/services/products/use-search';
 import { ApiContext, Product } from '@/types';
+import ProductCardList from '@/components/organisms/ProductCardList';
+import { Fragment } from 'react';
+import Link from 'next/link';
+import ProductCard from '@/components/organisms/ProductCard';
 
 const context: ApiContext = {
   apiRootUrl: process.env.NEXT_PUBLIC_API_BASE_PATH || '/api/proxy',
@@ -8,3 +13,32 @@ interface UserProductCardListContainerProps {
   userId: number;
   products?: Product[];
 }
+
+const UserProductCardListContainer = ({
+  userId,
+  products,
+}: UserProductCardListContainerProps) => {
+  const { products: userProducts } = useSearch(context, {
+    userId,
+    initial: products,
+  });
+
+  return (
+    <ProductCardList numberPerRow={6} numberPerRowForMobile={2}>
+      {userProducts.map((p) => (
+        <Fragment key={p.id}>
+          <Link href={`/products/${p.id}`} passHref>
+            <ProductCard
+              variant="small"
+              title={p.title}
+              price={p.price}
+              imageUrl={p.imageUrl}
+            />
+          </Link>
+        </Fragment>
+      ))}
+    </ProductCardList>
+  );
+};
+
+export default UserProductCardListContainer;

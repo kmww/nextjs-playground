50170480947fa2a2e2cf8c0d159283c1223479c4
kmw <kmw9708@gmail.com>
2023-11-13 16:06:10 +0900

    feat: UserPage

--- nextjs-wiki-book/src/pages/users/[id].tsx
+++ nextjs-wiki-book/src/pages/users/[id].tsx
@@ -1,8 +1,63 @@
+import BreadcrumbItem from '@/components/atoms/BreadcrumbItem';
+import Separator from '@/components/atoms/Sparator';
+import UserProductCardListContainer from '@/components/containers/UserProductCardListContainer';
+import UserProfileContainer from '@/components/containers/UserProfileContainer';
+import Box from '@/components/layout/Box';
+import Flex from '@/components/layout/Flex';
+import Breadcrumb from '@/components/molecules/Breadcrumb';
+import Layout from '@/components/templates/Layout';
+import getAllProducts from '@/services/products/get-all-products';
 import getUser from '@/services/users/get-users';
 import { ApiContext } from '@/types';
-import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
+import { GetStaticPropsContext, InferGetStaticPropsType, NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 type UserPageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const UserPage: NextPage<UserPageProps> = ({
+  id,
+  user,
+  products,
+}: UserPageProps) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Layout>
+      <Flex
+        paddingTop={2}
+        paddingRight={{ base: 2, md: 0 }}
+        paddingBottom={2}
+        paddingLeft={{ base: 2, md: 0 }}
+        justifyContent="center"
+      >
+        <Box width="1180px">
+          <Box marginBottom={2}>
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <Link href="/">홈</Link>
+              </BreadcrumbItem>
+              {user && <BreadcrumbItem>{user.username}</BreadcrumbItem>}
+            </Breadcrumb>
+          </Box>
+        </Box>
+        <Box>
+          <Box marginBottom={1}>
+            <UserProfileContainer userId={id} user={user} />
+          </Box>
+          <Box marginBottom={1}>
+            <Separator />
+          </Box>
+          <UserProductCardListContainer userId={id} products={products} />
+        </Box>
+      </Flex>
+    </Layout>
+  );
+};
 
 export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
   const context: ApiContext = {
@@ -27,3 +82,5 @@ export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
     },
   };
 };
+
+export default UserPage;

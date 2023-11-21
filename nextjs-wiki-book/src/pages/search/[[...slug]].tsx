18990a9c5fe5a08b55078e4e938cf6b9c545867d
kmw <kmw9708@gmail.com>
2023-11-21 21:34:56 +0900

    feat: search page anchor

---
+++ nextjs-wiki-book/src/pages/search/[[...slug]].tsx
@@ -0,0 +1,9 @@
+import Text from '@/components/atoms/Text';
+import styled from 'styled-components';
+
+const Anchor = styled(Text)`
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;

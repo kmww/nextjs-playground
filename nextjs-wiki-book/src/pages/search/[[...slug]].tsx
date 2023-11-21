01b41e770a3428d97f86cc8e5ac1420862f85ca1
kmw <kmw9708@gmail.com>
2023-11-21 21:39:14 +0900

    feat: search conditons handler

--- nextjs-wiki-book/src/pages/search/[[...slug]].tsx
+++ nextjs-wiki-book/src/pages/search/[[...slug]].tsx
@@ -1,5 +1,7 @@
 import Text from '@/components/atoms/Text';
-import { Category } from '@/types';
+import { Category, Condition } from '@/types';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
 const Anchor = styled(Text)`
@@ -13,4 +15,21 @@ const categoryNameDict: Record<Category, string> = {
   emoji: '이모티콘',
   figures: '피규어',
   pad: '마우스 패드',
+};
+
+const SearchPage: NextPage = () => {
+  const router = useRouter();
+  const slug: Category[] = Array.isArray(router.query.slug)
+    ? (router.query.slug as Category[])
+    : [];
+
+  const conditions = () => {
+    if (Array.isArray(router.query.condition)) {
+      return router.query.conditon as Condition[];
+    } else if (router.query.conditon) {
+      return [router.query.conditon as Condition[]];
+    } else {
+      return [];
+    }
+  };
 };

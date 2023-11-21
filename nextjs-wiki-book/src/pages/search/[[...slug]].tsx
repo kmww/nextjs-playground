17245f5ec97792f0a3f947cc5ba773539b7137a7
kmw <kmw9708@gmail.com>
2023-11-21 21:36:11 +0900

    feat: search category types

--- nextjs-wiki-book/src/pages/search/[[...slug]].tsx
+++ nextjs-wiki-book/src/pages/search/[[...slug]].tsx
@@ -1,4 +1,5 @@
 import Text from '@/components/atoms/Text';
+import { Category } from '@/types';
 import styled from 'styled-components';
 
 const Anchor = styled(Text)`
@@ -7,3 +8,9 @@ const Anchor = styled(Text)`
     text-decoration: underline;
   }
 `;
+
+const categoryNameDict: Record<Category, string> = {
+  emoji: '이모티콘',
+  figures: '피규어',
+  pad: '마우스 패드',
+};

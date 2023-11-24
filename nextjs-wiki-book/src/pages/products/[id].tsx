2a8219b9e1dedbf9fb7dfc009cf27bb10de22761
kmw <kmw9708@gmail.com>
2023-11-24 18:01:48 +0900

    feat: Products page

--- nextjs-wiki-book/src/pages/products/[id].tsx
+++ nextjs-wiki-book/src/pages/products/[id].tsx
@@ -1,11 +1,25 @@
+import BreadcrumbItem from '@/components/atoms/BreadcrumbItem';
+import Separator from '@/components/atoms/Sparator';
+import Text from '@/components/atoms/Text';
+import AddToCartButtonContainer from '@/components/containers/AddToCartButtonContainer';
+import Box from '@/components/layout/Box';
+import Flex from '@/components/layout/Flex';
+import Breadcrumb from '@/components/molecules/Breadcrumb';
+import ProductCard from '@/components/organisms/ProductCard';
+import UserProfile from '@/components/organisms/UserProfile';
+import Layout from '@/components/templates/Layout';
 import getAllProducts from '@/services/products/get-all-products';
 import getProduct from '@/services/products/get-product';
-import { ApiContext, Category } from '@/types';
+import useProduct from '@/services/products/use-product';
+import { ApiContext, Category, Product } from '@/types';
 import {
   GetStaticProps,
   GetStaticPropsContext,
   InferGetStaticPropsType,
+  NextPage,
 } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const categoryNameDict: Record<Category, string> = {
   emoji: '이모티콘',
@@ -15,6 +29,98 @@ const categoryNameDict: Record<Category, string> = {
 
 const context: ApiContext = {
   apiRootUrl: process.env.NEXT_PUBLIC_API_BASE_PATH || '/api/proxy',
+};
+
+type ProductPageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const ProductPage: NextPage<ProductPageProps> = ({
+  id,
+  product: initial,
+}: ProductPageProps) => {
+  const router = useRouter();
+  const data = useProduct(context, { id, initial });
+
+  const handleAddToCartButtonClick = () => {
+    router.push('/cart');
+  };
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const product = (data.product as Product) ?? initial;
+
+  return (
+    <Layout>
+      <Flex
+        paddingTop={2}
+        paddingBottom={2}
+        paddingLeft={{ base: 2, md: 0 }}
+        paddingRight={{ base: 2, md: 0 }}
+        justifyContent="center"
+        flexDirection={{ base: 'column', md: 'row' }}
+      >
+        <Box>
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link href="/">홈</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Link href="/search">검색</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Link href={`/search/${product.category}`}>
+                {categoryNameDict[product.category as Category]}
+              </Link>
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <Flex paddingTop={2} paddingBottom={1} justifyContent="center">
+            <ProductCard
+              variant="detail"
+              title={product.title}
+              price={product.price}
+              imageUrl={product.imageUrl}
+            />
+          </Flex>
+          <Separator />
+          <Box paddingTop={1}>
+            <Text as="h2" variant="large" marginTop={0}>
+              게시자
+            </Text>
+            <Link href={`users/${product.owner.id}`}>
+              <UserProfile
+                variant="small"
+                username={product.owner.username}
+                profileImageUrl={product.owner.profileImageUrl}
+                numberOfProducts={100}
+              />
+            </Link>
+          </Box>
+        </Box>
+        <Box padding={2} width={{ base: '100%', md: '700px' }}>
+          <Flex
+            justifyContent="space-between"
+            flexDirection="column"
+            height={{ base: '', md: '100%' }}
+          >
+            <Box>
+              {product.description
+                .split('\n')
+                .map((text: string, index: number) => (
+                  <Text key={index} as="p">
+                    {text}
+                  </Text>
+                ))}
+            </Box>
+            <AddToCartButtonContainer
+              product={product}
+              onAddToCartButtonClick={handleAddToCartButtonClick}
+            />
+          </Flex>
+        </Box>
+      </Flex>
+    </Layout>
+  );
 };
 
 export const getStaticPaths = async () => {
@@ -50,3 +156,5 @@ export const getStaticProps: GetStaticProps = async ({
     revalidate: 10,
   };
 };
+
+export default ProductPage;

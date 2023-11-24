11585f3ec7bb5dc893dc663e2d14cba4f409d3d1
kmw <kmw9708@gmail.com>
2023-11-24 18:00:32 +0900

    feat: AddToCartButtonContainer

--- nextjs-wiki-book/src/components/containers/AddToCartButtonContainer.tsx
+++ nextjs-wiki-book/src/components/containers/AddToCartButtonContainer.tsx
@@ -1,6 +1,38 @@
+import { useShoppingCartContext } from '@/contexts/ShoppingCartContext';
 import { Product } from '@/types';
+import Button from '@/components/atoms/Button';
 
 interface AddToCartButtonContainerProps {
   product: Product;
   onAddToCartButtonClick?: (product: Product) => void;
 }
+
+const AddToCartButtonContainer = ({
+  product,
+  onAddToCartButtonClick,
+}: AddToCartButtonContainerProps) => {
+  const { cart, addProductToCart } = useShoppingCartContext();
+
+  const handleAddToCartButtonClick = () => {
+    const productId = Number(product.id);
+    const result = cart.findIndex((item) => item.id === productId);
+
+    if (result === -1) {
+      addProductToCart(product);
+    }
+
+    onAddToCartButtonClick && onAddToCartButtonClick(product);
+  };
+
+  return (
+    <Button
+      width={{ base: '100%', md: '400px' }}
+      height="66px"
+      onClick={handleAddToCartButtonClick}
+    >
+      카트에 추가
+    </Button>
+  );
+};
+
+export default AddToCartButtonContainer;

98af3d35f32f2f80c84161e1ed32ff8832b346b4
kmw <kmw9708@gmail.com>
2023-11-27 13:45:00 +0900

    feat: AuthGuard hooks

---
+++ nextjs-wiki-book/src/utils/hooks.ts
@@ -0,0 +1,21 @@
+import { useAuthContext } from '@/contexts/AuthContext';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export const useAuthGuard = (): void => {
+  const router = useRouter();
+  const { authUser, isLoading } = useAuthContext();
+
+  useEffect(() => {
+    if (!authUser && !isLoading) {
+      const currentPath = router.pathname;
+
+      router.push({
+        pathname: '/signin',
+        query: {
+          redirect_to: currentPath,
+        },
+      });
+    }
+  }, [router, authUser, isLoading]);
+};

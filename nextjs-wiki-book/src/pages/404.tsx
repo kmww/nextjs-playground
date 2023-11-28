7ae087b949a035e3fae6c83e44675191e039fc1e
kmw <kmw9708@gmail.com>
2023-11-28 19:51:54 +0900

    feat: 404page

---
+++ nextjs-wiki-book/src/pages/404.tsx
@@ -0,0 +1,11 @@
+import Text from '@/components/atoms/Text';
+
+const NotFoundPage = () => {
+  return (
+    <Text as="h1" variant="extraLarge">
+      404- Page Not Found
+    </Text>
+  );
+};
+
+export default NotFoundPage;

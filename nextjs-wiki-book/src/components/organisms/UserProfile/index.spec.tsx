5578a2792a42fa52268c7e2c1f947b6422dd17e0
kmw <kmw9708@gmail.com>
2023-12-02 20:29:24 +0900

    test: UserProfile dummyUser

---
+++ nextjs-wiki-book/src/components/organisms/UserProfile/index.spec.tsx
@@ -0,0 +1,9 @@
+import { User } from '@/types';
+import { RenderResult } from '@testing-library/react';
+
+const dummyUserData = {
+  username: 'Test User',
+  profileImageUrl: '/images/example/1.jpg',
+  numberOfProducts: 5,
+  description: '',
+};

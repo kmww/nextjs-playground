667d490e9f39da3c70302d6a01c6004ce7d00c57
kmw <kmw9708@gmail.com>
2023-12-02 16:38:26 +0900

    test: Header 상품 존재시 배지 표시 테스트

--- nextjs-wiki-book/src/components/organisms/Header/index.spec.tsx
+++ nextjs-wiki-book/src/components/organisms/Header/index.spec.tsx
@@ -1,10 +1,17 @@
+import { AuthContextProvider } from '@/contexts/AuthContext';
+import { theme } from '@/styles/themes';
+import { Product, User } from '@/types';
+import { RenderResult, render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import Header from './';
+
 jest.mock('contexts/shoppingCartContext');
 import { useShoppingCartContext } from '@/contexts/ShoppingCartContext';
-import { Product, User } from '@/types';
 const { ShoppingCartContextProvider } = jest.requireActual(
   'contexts/ShoppingCartContext',
 );
 
+// 더미 사용자
 const authUser: User = {
   id: 1,
   username: 'T.S',
@@ -14,6 +21,7 @@ const authUser: User = {
   profileImageUrl: '/images/sample/1.jpg',
 };
 
+// 더미 상품
 const product: Product = {
   id: 1,
   category: 'emoji',
@@ -25,3 +33,37 @@ const product: Product = {
   condition: 'new',
   owner: authUser,
 };
+
+describe('Header', () => {
+  let renderResult: RenderResult;
+  const useShoppingCartContextMock =
+    useShoppingCartContext as jest.MockedFunction<
+      typeof useShoppingCartContext
+    >;
+
+  it('카트에 상품이 존재한다.', async () => {
+    useShoppingCartContextMock.mockReturnValue({
+      cart: [product],
+      addProductToCart: () => {},
+      removeProductFromCart: () => {},
+    });
+
+    renderResult = render(
+      <ThemeProvider theme={theme}>
+        <ShoppingCartContextProvider>
+          <AuthContextProvider
+            authUser={authUser}
+            context={{ apiRootUrl: 'https://dummy' }}
+          >
+            <Header />
+          </AuthContextProvider>
+        </ShoppingCartContextProvider>
+      </ThemeProvider>,
+    );
+    // 카트에 상품 존재시 배지가 표시된다.
+    expect(screen.getAllByTestId('badge-wrapper').length).toBeGreaterThan(0);
+
+    renderResult.unmount();
+    useShoppingCartContextMock.mockReset();
+  });
+});

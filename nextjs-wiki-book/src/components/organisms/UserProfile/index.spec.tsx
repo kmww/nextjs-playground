d72629d4a6be864bac962a264a306d82c218dd99
kmw <kmw9708@gmail.com>
2023-12-02 20:35:36 +0900

    test: UserProfile 게시물 작성 수 확인

--- nextjs-wiki-book/src/components/organisms/UserProfile/index.spec.tsx
+++ nextjs-wiki-book/src/components/organisms/UserProfile/index.spec.tsx
@@ -1,5 +1,5 @@
-import { User } from '@/types';
-import { RenderResult } from '@testing-library/react';
+import { RenderResult, render, screen } from '@testing-library/react';
+import UserProfile from './';
 
 const dummyUserData = {
   username: 'Test User',
@@ -7,3 +7,15 @@ const dummyUserData = {
   numberOfProducts: 5,
   description: '',
 };
+
+describe('UserProfile', () => {
+  let renderResult: RenderResult;
+
+  it('사용자의 게시물 작성 수를 렌더링한다.', () => {
+    renderResult = render(<UserProfile {...dummyUserData} />);
+
+    expect(screen.getByText(/제품 게시 완료/)).toHaveTextContent(
+      '5개의 제품 게시 완료',
+    );
+  });
+});

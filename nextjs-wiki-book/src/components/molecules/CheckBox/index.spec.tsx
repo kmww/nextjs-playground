6893bcd4daaff0ae89dcef8f92bc914065fa4bc9
kmw <kmw9708@gmail.com>
2023-12-02 17:43:13 +0900

    test: checkbox 클릭시 onChange 호출

--- nextjs-wiki-book/src/components/molecules/CheckBox/index.spec.tsx
+++ nextjs-wiki-book/src/components/molecules/CheckBox/index.spec.tsx
@@ -10,6 +10,21 @@ import { theme } from '@/styles/themes';
 
 describe('CheckBox', () => {
   let renderResult: RenderResult;
+  let handleChange: jest.Mock;
+
+  it('클릭시 onChange가 호출된다.', () => {
+    handleChange = jest.fn();
+    renderResult = render(
+      <ThemeProvider theme={theme}>
+        <CheckBox onChange={handleChange} />
+      </ThemeProvider>,
+    );
+
+    fireEvent.click(screen.getByTestId('checkbox-wrapper'));
+    expect(handleChange).toHaveBeenCalledTimes(1);
+
+    renderResult.unmount();
+  });
 
   it('토글 버튼 클릭시 체크 상태가 변경된다.', () => {
     renderResult = render(
@@ -23,5 +38,7 @@ describe('CheckBox', () => {
     expect(element.checked).toBe(false);
     fireEvent.click(element);
     expect(element.checked).toBe(true);
+
+    renderResult.unmount();
   });
 });

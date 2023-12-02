f2c690037265bf7b39e3945a5e0816db92c9ea5b
kmw <kmw9708@gmail.com>
2023-12-02 16:31:16 +0900

    test: Header user&product mock

--- nextjs-wiki-book/src/components/organisms/Header/index.spec.tsx
+++ nextjs-wiki-book/src/components/organisms/Header/index.spec.tsx
@@ -1,5 +1,27 @@
 jest.mock('contexts/shoppingCartContext');
 import { useShoppingCartContext } from '@/contexts/ShoppingCartContext';
+import { Product, User } from '@/types';
 const { ShoppingCartContextProvider } = jest.requireActual(
   'contexts/ShoppingCartContext',
 );
+
+const authUser: User = {
+  id: 1,
+  username: 'T.S',
+  displayName: 'Test User',
+  email: 'test@test.com',
+  description: '',
+  profileImageUrl: '/images/sample/1.jpg',
+};
+
+const product: Product = {
+  id: 1,
+  category: 'emoji',
+  title: 'Product',
+  description: '',
+  imageUrl: '/images/sample/1.jpg',
+  blurDataUrl: '',
+  price: 3000,
+  condition: 'new',
+  owner: authUser,
+};

9596821b4dc7e6458606726a6897b7bbcf727ed6
kmw <kmw9708@gmail.com>
2023-12-04 15:23:27 +0900

    test: CartProduct 카트 제품 렌더링 테스트

--- nextjs-wiki-book/src/components/organisms/CartProduct/index.spec.tsx
+++ nextjs-wiki-book/src/components/organisms/CartProduct/index.spec.tsx
@@ -1,10 +1,31 @@
+import { RenderResult, render, screen } from '@testing-library/react';
 import type { CartProductProps } from './';
+import CartProduct from './';
 
 const product: CartProductProps = {
   id: 1,
   imageUrl: '/images/example/1.jpg',
   price: 1000,
   title: 'Test Product',
-  onBuyButtonClick: jest.fn(),
-  onRemoveButtonClick: jest.fn(),
 };
+
+describe('ProductCart', () => {
+  let renderResult: RenderResult;
+  let onBuyButtonClick: jest.Mock;
+  let onRemoveButtonClick: jest.Mock;
+
+  afterEach(() => {
+    renderResult.unmount();
+  });
+
+  it('카트에 제품이 올바르게 렌더링된다.', () => {
+    renderResult = render(<CartProduct {...product} />);
+
+    // 이미지 alt
+    expect(screen.getByAltText('Test Product')).toBeInTheDocument();
+    // 제품 제목
+    expect(screen.getByText('Test Product')).toBeInTheDocument();
+    // 제품 가격
+    expect(screen.getByText('1000원')).toBeInTheDocument();
+  });
+});

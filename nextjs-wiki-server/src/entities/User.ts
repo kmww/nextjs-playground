593929f4fc4e084a25ac81b9d0b05234ae28e95f
kmw <kmw9708@gmail.com>
2023-12-12 01:26:47 +0900

    feat: 유저 타입

---
+++ nextjs-wiki-server/src/entities/User.ts
@@ -0,0 +1,22 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+
+@ObjectType()
+export class User {
+  @Field(() => Int, { description: '유저 아이디' })
+  id: number;
+
+  @Field({ description: '유저 이름' })
+  username: string;
+
+  @Field({ description: '유저 닉네임' })
+  displayName: string;
+
+  @Field({ description: '이메일' })
+  email: string;
+
+  @Field({ nullable: true, description: '프로필 사진' })
+  profileImageUrl?: string;
+
+  @Field({ description: '유저 소개' })
+  description: string;
+}

1bddd0f67aead74e7464804434fb342aaf6eec26
kmw <kmw9708@gmail.com>
2023-12-12 00:50:21 +0900

    feat: apollo 엔트리 파일

---
+++ nextjs-wiki-server/src/index.ts
@@ -0,0 +1,41 @@
+import 'reflect-metadata';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ApolloServer, gql } from 'apollo-server-express';
+import express from 'express';
+import http from 'http';
+
+async function main() {
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    typeDefs: gql`
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => `Hello!`,
+      },
+    },
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  const httpServer = http.createServer(app);
+
+  httpServer.listen(process.env.PORT || 4000, () => {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`
+        sserver started on => http://localhost:4000
+        graphql playground => http://localhost:4000/graphql
+      `);
+    } else {
+      console.log(`
+      Production server Started...`);
+    }
+  });
+}
+
+main().catch((error) => console.error(error));

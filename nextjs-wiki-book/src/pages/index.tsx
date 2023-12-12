a5be75f30adccf13841db3c5196ca8485896aad3
kmw <kmw9708@gmail.com>
2023-12-12 22:04:44 +0900

    fix: 메인페이지 레이아웃 수정

--- nextjs-wiki-book/src/pages/index.tsx
+++ nextjs-wiki-book/src/pages/index.tsx
@@ -39,40 +39,42 @@ const Home = ({
 
   return (
     <Layout>
-      <Flex
-        width={{ base: '100%', md: '1040px' }}
-        justifyContent="center"
-        alignItems="center"
-        flexDirection={{ base: 'column', md: 'row' }}
-      >
-        <Box width="100%">
-          <Text as="h1" marginBottom={0} color="white" variant="extraLarge">
-            LOA C2C에서
-          </Text>
-          <Text as="h1" marginTop={0} color="white" variant="extraLarge">
-            마음에 드는 아이템을 찾자
-          </Text>
-        </Box>
-        <Box width="100%">
-          <Text as="p" color="white" variant="mediumLarge">
-            LOA C@C는 데모 애플리케이션 입니다. 목 서버를 사용하며 소스코드는
-            <Text
-              as="a"
-              style={{ textDecoration: 'underline' }}
-              target="_blank"
-              href="https://github.com/kmww/nextjs-playground/tree/main/nextjs-wiki-book"
-              variant="mediumLarge"
-              color="white"
-            >
-              다음
+      <Flex padding={2} justifyContent="center" backgroundColor="primary">
+        <Flex
+          width={{ base: '100%', md: '1040px' }}
+          justifyContent="center"
+          alignItems="center"
+          flexDirection={{ base: 'column', md: 'row' }}
+        >
+          <Box width="100%">
+            <Text as="h1" marginBottom={0} color="white" variant="extraLarge">
+              LOA C2C에서
             </Text>
-            의 Github에서 확인 할 수 있습니다.
-          </Text>
-          <Text as="p" color="white" variant="mediumLarge">
-            이 애플리케이션은 Next.js/TypeScript로 제작되었으며, 백엔드의 목
-            API는 json-server가 사용되고 있습니다.
-          </Text>
-        </Box>
+            <Text as="h1" marginTop={0} color="white" variant="extraLarge">
+              마음에 드는 아이템을 찾자
+            </Text>
+          </Box>
+          <Box width="100%">
+            <Text as="p" color="white" variant="mediumLarge">
+              LOA C@C는 데모 애플리케이션 입니다. 목 서버를 사용하며 소스코드는
+              <Text
+                as="a"
+                style={{ textDecoration: 'underline' }}
+                target="_blank"
+                href="https://github.com/kmww/nextjs-playground/tree/main/nextjs-wiki-book"
+                variant="mediumLarge"
+                color="white"
+              >
+                다음
+              </Text>
+              의 Github에서 확인 할 수 있습니다.
+            </Text>
+            <Text as="p" color="white" variant="mediumLarge">
+              이 애플리케이션은 Next.js/TypeScript로 제작되었으며, 백엔드의 목
+              API는 Graphql이 사용되고 있습니다.
+            </Text>
+          </Box>
+        </Flex>
       </Flex>
       <Flex paddingBottom={2} justifyContent="center">
         <Box

e590bab1d880e6eaa65fa909da0ca27383e243d0
kmw <kmw9708@gmail.com>
2023-12-13 20:55:10 +0900

    feat: product 상세 정보 리졸버

--- nextjs-wiki-server/src/resolvers/Product.ts
+++ nextjs-wiki-server/src/resolvers/Product.ts
@@ -1,11 +1,16 @@
 import dummyProducts from '../data/productdata';
 import { Product } from '../entities/Product';
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Int, Query, Resolver } from 'type-graphql';
 
 @Resolver(Product)
 export class ProductResolver {
   @Query(() => [Product])
   products(): Product[] {
     return dummyProducts.products;
+  }
+
+  @Query(() => Product, { nullable: true })
+  product(@Arg('productId', () => Int) productId: number): Product | undefined {
+    return dummyProducts.products.find((product) => product.id === productId);
   }
 }

2a4c35bc018adb2389e1da1892abbedce1a35b85
kmw <kmw9708@gmail.com>
2023-12-15 23:42:42 +0900

    feat: 제품 검색 쿼리

--- nextjs-wiki-server/src/resolvers/Product.ts
+++ nextjs-wiki-server/src/resolvers/Product.ts
@@ -1,7 +1,9 @@
 import dummyProducts from '../data/productdata';
 import { Product } from '../entities/Product';
-import { Arg, Int, Query, Resolver } from 'type-graphql';
+import { Arg, Field, Int, ObjectType, Query, Resolver } from 'type-graphql';
 
+type Category = 'emoji' | 'figures' | 'pad';
+type Condition = 'new' | 'used';
 @Resolver(Product)
 export class ProductResolver {
   @Query(() => [Product])
@@ -12,5 +14,28 @@ export class ProductResolver {
   @Query(() => Product, { nullable: true })
   product(@Arg('productId', () => Int) productId: number): Product | undefined {
     return dummyProducts.products.find((product) => product.id === productId);
+  }
+
+  @Query(() => [Product])
+  searchProducts(
+    @Arg('category', { nullable: true })
+    category: Category,
+    @Arg('conditions', { nullable: true })
+    conditions: Condition,
+  ): Product[] | [] {
+    if (category) {
+      let products = dummyProducts.products.filter(
+        (product) => product.category === category,
+      );
+      if (conditions) {
+        products = products.filter(
+          (product) => product.condition === conditions,
+        );
+        return products;
+      } else {
+        return products;
+      }
+    }
+    return [];
   }
 }

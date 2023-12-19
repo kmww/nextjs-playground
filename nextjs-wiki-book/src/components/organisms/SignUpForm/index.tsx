0a7fde84df7bbda65a9ddf42f5d3ac70570e07d1
kmw <kmw9708@gmail.com>
2023-12-19 23:29:47 +0900

    feat: SignUpForm

--- nextjs-wiki-book/src/components/organisms/SignUpForm/index.tsx
+++ nextjs-wiki-book/src/components/organisms/SignUpForm/index.tsx
@@ -1,4 +1,8 @@
+import { FormControl, FormLabel } from '@mui/material';
 import { useForm } from 'react-hook-form';
+import Button from '@/components/atoms/Button';
+import Input from '@/components/atoms/Input';
+import Text from '@/components/atoms/Text';
 
 interface SignUpFormData {
   email: string;
@@ -28,6 +32,74 @@ const SignUpForm = ({ onSignUp }: SignUpFormProps) => {
 
     onSignUp && onSignUp(email, username, password, displayName);
   };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl fullWidth sx={{ paddingBottom: 1 }}>
+        <FormLabel sx={{ fontWeight: 'bold' }}>이메일</FormLabel>
+        <Input
+          {...register('email', { required: true })}
+          name="email"
+          type="text"
+          placeholder="example@example.com"
+          hasError={!!errors.email}
+        />
+        {errors.email && (
+          <Text color="danger" variant="small" paddingLeft={1}>
+            이메일을 입력해주세요
+          </Text>
+        )}
+      </FormControl>
+      <FormControl fullWidth sx={{ paddingBottom: 1 }}>
+        <FormLabel sx={{ fontWeight: 'bold' }}>비밀번호</FormLabel>
+        <Input
+          {...register('password', { required: true })}
+          name="password"
+          type="password"
+          placeholder="8자 이상의 영문,숫자,특문"
+          hasError={!!errors.password}
+        />
+        {errors.password && (
+          <Text color="danger" variant="small" paddingLeft={1}>
+            비밀번호를 입력해주세요
+          </Text>
+        )}
+      </FormControl>
+      <FormControl fullWidth sx={{ marginBottom: 1 }}>
+        <FormLabel sx={{ fontWeight: 'bold' }}>이름</FormLabel>
+        <Input
+          {...register('username', { required: true })}
+          name="username"
+          type="text"
+          placeholder="홍길동"
+          hasError={!!errors.username}
+        />
+        {errors.username && (
+          <Text color="danger" variant="small" paddingLeft={1}>
+            실명을 입력해주세요
+          </Text>
+        )}
+      </FormControl>
+      <FormControl fullWidth sx={{ marginBottom: 2 }}>
+        <FormLabel sx={{ fontWeight: 'bold' }}>닉네임</FormLabel>
+        <Input
+          {...register('displayName', { required: true })}
+          name="displayName"
+          type="text"
+          placeholder="닉네임"
+          hasError={!!errors.displayName}
+        />
+        {errors.displayName && (
+          <Text color="danger" variant="small" paddingLeft={1}>
+            닉네임을 입력해주세요
+          </Text>
+        )}
+      </FormControl>
+      <Button width="100%" type="submit">
+        회원가입
+      </Button>
+    </form>
+  );
 };
 
 export default SignUpForm;

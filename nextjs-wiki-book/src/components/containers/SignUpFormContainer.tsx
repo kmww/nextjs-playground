9bd7502c3267ba034ccb2aa4ab2a23516222112d
kmw <kmw9708@gmail.com>
2023-12-22 00:55:59 +0900

    feat: SignUpFormContainer

---
+++ nextjs-wiki-book/src/components/containers/SignUpFormContainer.tsx
@@ -0,0 +1,32 @@
+import { useRouter } from 'next/router';
+import SignUpForm from '@/components/organisms/SignUpForm';
+import { useGlobalSpinnerActionsContext } from '@/contexts/GlobalSpinnerContext';
+import { SignUpInput, useSignUpMutation } from '@/generated/graphql';
+
+const SignUpFormContainer = () => {
+  const [signup, { loading }] = useSignUpMutation();
+  const setGlobalSpinner = useGlobalSpinnerActionsContext();
+  const router = useRouter();
+
+  const handleSubmit = async (signUpInput: SignUpInput) => {
+    try {
+      setGlobalSpinner(true);
+      const res = await signup({ variables: { signUpInput } });
+      if (res.data?.signUp) {
+        await router.replace('/');
+        window.alert('회원가입이 완료되었습니다.');
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        window.alert(`회원가입 도중 문제가 발생했습니다.
+error: ${error?.message}`);
+      }
+    } finally {
+      setGlobalSpinner(false);
+    }
+  };
+
+  return <SignUpForm onSignUp={handleSubmit} isLoading={loading} />;
+};
+
+export default SignUpFormContainer;

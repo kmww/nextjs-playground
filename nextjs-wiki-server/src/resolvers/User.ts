f8eaa5478a9863a6c31b23aaa70592a12fa65546
kmw <kmw9708@gmail.com>
2023-12-26 22:05:19 +0900

    feat: 로그인 뮤테이션

--- nextjs-wiki-server/src/resolvers/User.ts
+++ nextjs-wiki-server/src/resolvers/User.ts
@@ -24,12 +24,14 @@ export class SignUpInput {
 @InputType({ description: '로그인 인풋 데이터' })
 export class LoginInput {
   @Field() @IsString() emailOrDisplayName: string;
+
   @Field() @IsString() password: string;
 }
 
 @ObjectType({ description: '필드 에러 타입' })
 class FieldError {
   @Field() field: string;
+
   @Field() message: string;
 }
 
@@ -63,5 +65,38 @@ export class UserResolver {
 
     await UserData.insert(newUser);
     return newUser;
+  }
+
+  @Mutation(() => LoginResponse)
+  public async login(
+    @Arg('loginInput') loginInput: LoginInput,
+  ): Promise<LoginResponse> {
+    const { emailOrDisplayName, password } = loginInput;
+
+    const user = await UserData.findOne({
+      where: [
+        { email: emailOrDisplayName },
+        { displayName: emailOrDisplayName },
+      ],
+    });
+
+    if (!user) {
+      return {
+        errors: [
+          { field: 'emailOrDisplayName', message: '해당하는 유저가 없습니다' },
+        ],
+      };
+    }
+
+    const isValid = await argon2.verify(user.password, password);
+    if (!isValid) {
+      return {
+        errors: [
+          { field: 'password', message: '비밀번호를 올바르게 입력해주세요.' },
+        ],
+      };
+    }
+
+    return { user };
   }
 }

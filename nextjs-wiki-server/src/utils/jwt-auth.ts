4ace41339343c9aba7274fff12d881dd0af9a1b3
kmw <kmw9708@gmail.com>
2023-12-27 19:33:37 +0900

    feat: 액세스토큰 생성

---
+++ nextjs-wiki-server/src/utils/jwt-auth.ts
@@ -0,0 +1,19 @@
+import UserData from 'src/entities/UserData';
+import jwt from 'jsonwebtoken';
+
+export const DEFAULT_JWT_SECRET_KEY = 'secret-key';
+
+export interface JwtVerifiedUser {
+  userId: UserData['id'];
+}
+
+export const createAccessToken = (user: UserData): string => {
+  const userData: JwtVerifiedUser = { userId: user.id };
+  const accessToken = jwt.sign(
+    userData,
+    process.env.JWT_SECRET_KEY || DEFAULT_JWT_SECRET_KEY,
+    { expiresIn: '30m' },
+  );
+
+  return accessToken;
+};

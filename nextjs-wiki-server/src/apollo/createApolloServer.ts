e408462ab185a309f9ca80225f4ee2806c5ebf19
kmw <kmw9708@gmail.com>
2023-12-28 20:35:45 +0900

    feat: apolloserver context설정

--- nextjs-wiki-server/src/apollo/createApolloServer.ts
+++ nextjs-wiki-server/src/apollo/createApolloServer.ts
@@ -3,13 +3,21 @@ import { ProductResolver } from '../resolvers/Product';
 import { UserResolver } from '../resolvers/User';
 import { buildSchema } from 'type-graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { Request, Response } from 'express';
+export interface MyContext {
+  req: Request;
+  res: Response;
+}
 
 const createApolloServer = async (): Promise<ApolloServer> => {
-  return new ApolloServer({
+  return new ApolloServer<MyContext>({
     schema: await buildSchema({
       resolvers: [ProductResolver, UserResolver],
     }),
     plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    context: ({ req, res }) => {
+      return { req, res };
+    },
   });
 };
 

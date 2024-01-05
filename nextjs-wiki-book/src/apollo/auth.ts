1707eea26cdf665dd7ce40cfd2989d1fefab56a2
kmw <kmw9708@gmail.com>
2024-01-05 22:06:52 +0900

    feat: 토큰 새로고침 함수

---
+++ nextjs-wiki-book/src/apollo/auth.ts
@@ -0,0 +1,35 @@
+import { ApolloClient, NormalizedCacheObject, Operation } from '@apollo/client';
+import {
+  RefreshAccessTokenDocument,
+  RefreshAccessTokenMutation,
+} from '@/generated/graphql';
+
+export const refreshAccessToekn = (
+  _apolloClient: ApolloClient<NormalizedCacheObject>,
+  operation: Operation,
+): Promise<boolean> =>
+  _apolloClient
+    .mutate<RefreshAccessTokenMutation>({
+      mutation: RefreshAccessTokenDocument,
+    })
+    .then(({ data }) => {
+      const newAccessToken = data?.refreshAccessToken?.accessToken;
+      if (!newAccessToken) {
+        localStorage.setItem('access_token', '');
+        return false;
+      }
+
+      localStorage.setItem('access_token', newAccessToken);
+      const prevContext = operation.getContext();
+      operation.setContext({
+        headers: {
+          ...prevContext.headers,
+          authorization: `Bearer ${newAccessToken}`,
+        },
+      });
+      return true;
+    })
+    .catch(() => {
+      localStorage.setItem('access_token', '');
+      return false;
+    });

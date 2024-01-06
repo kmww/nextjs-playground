2d87fd76f10dbbdb408b21be582ff68a9588816f
kmw <kmw9708@gmail.com>
2024-01-06 14:58:39 +0900

    feat: 토큰 만료시 리프레시토큰 뮤테이션 실행

--- nextjs-wiki-book/src/apollo/createApolloClient.ts
+++ nextjs-wiki-book/src/apollo/createApolloClient.ts
@@ -3,9 +3,11 @@ import {
   HttpLink,
   NormalizedCacheObject,
   from,
+  fromPromise,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
+import { refreshAccessToekn } from './auth';
 import { createApolloCache } from './createApolloCache';
 
 let apolloClient: ApolloClient<NormalizedCacheObject>;
@@ -19,21 +21,31 @@ export const createApolloClient = (): ApolloClient<NormalizedCacheObject> => {
   return apolloClient;
 };
 
-const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
-  if (graphQLErrors) {
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: -> ${operation.operationName}
-Message: ${message}, Query: ${path}, Location: ${JSON.stringify(locations)}`,
-      ),
-    );
-  }
+const errorLink = onError(
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors) {
+      if (
+        graphQLErrors.find((error) => error.message === 'access token expired')
+      ) {
+        return fromPromise(refreshAccessToekn(apolloClient, operation))
+          .filter((result) => !!result)
+          .flatMap(() => forward(operation));
+      }
 
-  if (networkError) {
-    console.log(`[networkError]: -> ${operation.operationName}
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: -> ${operation.operationName}
+Message: ${message}, Query: ${path}, Location: ${JSON.stringify(locations)}`,
+        ),
+      );
+    }
+
+    if (networkError) {
+      console.log(`[networkError]: -> ${operation.operationName}
 Message: ${networkError.message}`);
-  }
-});
+    }
+  },
+);
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql',

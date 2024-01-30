a43a578a24b0b36ec58aa8b0a907faa7a86281a8
kmw <kmw9708@gmail.com>
2024-01-30 17:51:22 +0900

    feat: Nav Menu

--- nextjs-wiki-book/src/components/organisms/Header/LoggedInMenu.tsx
+++ nextjs-wiki-book/src/components/organisms/Header/LoggedInMenu.tsx
@@ -1,5 +1,6 @@
 import { useApolloClient } from '@apollo/client';
-import { useMemo } from 'react';
+import { IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
+import { useMemo, useState } from 'react';
 import { Anchor, NavLink } from './';
 import { LogoutIcon } from '@/components/atoms/IconButton';
 import ShapeImage from '@/components/atoms/ShapeImage';
@@ -17,9 +18,19 @@ interface LoggedInMenuProps {
 
 const LoggedInMenu = ({ isAuth }: LoggedInMenuProps) => {
   const client = useApolloClient();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
   const { data } = useMeQuery({ skip: !isAuth });
   const [logout, { loading: logoutLoading }] = useLogoutMutation();
   const [upload] = useUploadProfileImageMutation();
+
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const profileImage = useMemo(() => {
     if (data?.me?.profileImageUrl) {
@@ -53,23 +64,60 @@ const LoggedInMenu = ({ isAuth }: LoggedInMenuProps) => {
   return (
     <>
       <NavLink>
-        <Box>
-          <input
-            id="upload-profile-image"
-            type="file"
-            accept="image/*"
-            hidden
-            onChange={handleImageUpload}
-          />
-          <ShapeImage
-            shape="circle"
-            src={profileImage}
-            width={24}
-            height={24}
-            data-testid="profile-shape-image"
-            alt={profileImage}
-          />
-        </Box>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <ShapeImage
+              shape="circle"
+              src={profileImage}
+              width={24}
+              height={24}
+              data-testid="profile-shape-image"
+              alt={profileImage}
+            />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&::before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        ></Menu>
       </NavLink>
       <NavLink>
         <Anchor>

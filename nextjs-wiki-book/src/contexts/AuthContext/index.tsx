9626111a3a6332d82ae1dce898036b127a6ea2ae
kmw <kmw9708@gmail.com>
2024-03-25 23:02:33 +0900

    refactor: AuthContext 뮤테이션 수정

--- nextjs-wiki-book/src/contexts/AuthContext/index.tsx
+++ nextjs-wiki-book/src/contexts/AuthContext/index.tsx
@@ -1,22 +1,83 @@
+// import React, { useContext } from 'react';
+// import useSWR from 'swr';
+// import signin from '@/services/auth/signin';
+// import signout from '@/services/auth/signout';
+// import type { ApiContext, User } from '@/types';
+
 import React, { useContext } from 'react';
-import useSWR from 'swr';
-import signin from '@/services/auth/signin';
-import signout from '@/services/auth/signout';
-import type { ApiContext, User } from '@/types';
+import { useLoginMutation, useLogoutMutation } from '@/generated/graphql';
+import { User } from '@/types';
+
+// interface AuthContextType {
+//   authUser?: User;
+//   isLoading: boolean;
+//   signin: (username: string, password: string) => Promise<void>;
+//   signout: () => Promise<void>;
+//   mutate: (
+//     data?: User | Promise<User>,
+//     shouldRevalidate?: boolean,
+//   ) => Promise<User | undefined>;
+// }
+
+// interface AuthContextProviderProps {
+//   context: ApiContext;
+//   authUser?: User;
+// }
+
+// const AuthContext = React.createContext<AuthContextType>({
+//   authUser: undefined,
+//   isLoading: false,
+//   signin: async () => Promise.resolve(),
+//   signout: async () => Promise.resolve(),
+//   mutate: async () => Promise.resolve(undefined),
+// });
+
+// export const useAuthContext = (): AuthContextType =>
+//   useContext<AuthContextType>(AuthContext);
+
+// export const AuthContextProvider = ({
+//   context,
+//   authUser,
+//   children,
+// }: React.PropsWithChildren<AuthContextProviderProps>) => {
+//   const { data, error, mutate } = useSWR<User>(
+//     `${context.apiRootUrl.replace(/\/$/g, '')}/users/me`,
+//   );
+//   const isLoading = !data && !error;
+
+//   const signinInternal = async (username: string, password: string) => {
+//     await signin(context, { username, password });
+//     await mutate();
+//   };
+
+//   const signoutInternal = async () => {
+//     await signout(context);
+//     await mutate();
+//   };
+
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         authUser: data ?? authUser,
+//         isLoading,
+//         signin: signinInternal,
+//         signout: signoutInternal,
+//         mutate,
+//       }}
+//     >
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
 
 interface AuthContextType {
   authUser?: User;
   isLoading: boolean;
   signin: (username: string, password: string) => Promise<void>;
   signout: () => Promise<void>;
-  mutate: (
-    data?: User | Promise<User>,
-    shouldRevalidate?: boolean,
-  ) => Promise<User | undefined>;
 }
 
 interface AuthContextProviderProps {
-  context: ApiContext;
   authUser?: User;
 }
 
@@ -25,40 +86,41 @@ const AuthContext = React.createContext<AuthContextType>({
   isLoading: false,
   signin: async () => Promise.resolve(),
   signout: async () => Promise.resolve(),
-  mutate: async () => Promise.resolve(undefined),
 });
 
 export const useAuthContext = (): AuthContextType =>
   useContext<AuthContextType>(AuthContext);
 
 export const AuthContextProvider = ({
-  context,
   authUser,
   children,
 }: React.PropsWithChildren<AuthContextProviderProps>) => {
-  const { data, error, mutate } = useSWR<User>(
-    `${context.apiRootUrl.replace(/\/$/g, '')}/users/me`,
-  );
-  const isLoading = !data && !error;
+  const [loginMutation, { loading: loginLoading }] = useLoginMutation();
+  const [logoutMutation, { loading: logoutLoading }] = useLogoutMutation();
+  const isLoading = loginLoading || logoutLoading;
 
-  const signinInternal = async (username: string, password: string) => {
-    await signin(context, { username, password });
-    await mutate();
+  const signinInternal = async (
+    emailOrDisplayName: string,
+    password: string,
+  ) => {
+    await loginMutation({
+      variables: {
+        loginInput: { emailOrDisplayName, password },
+      },
+    });
   };
 
   const signoutInternal = async () => {
-    await signout(context);
-    await mutate();
+    await logoutMutation();
   };
 
   return (
     <AuthContext.Provider
       value={{
-        authUser: data ?? authUser,
+        authUser,
         isLoading,
         signin: signinInternal,
         signout: signoutInternal,
-        mutate,
       }}
     >
       {children}

9dcfdb5e9d32d0f2336e0ec3703036561c127138
kmw <kmw9708@gmail.com>
2024-03-26 20:34:08 +0900

    feat: 판매 뮤테이션 및 실제 더미->실제 데이터

--- nextjs-wiki-server/src/resolvers/Product.ts
+++ nextjs-wiki-server/src/resolvers/Product.ts
@@ -1,42 +1,89 @@
-import dummyProducts from '../data/productdata';
-import { Product } from '../entities/Product';
-import { Arg, Int, Query, Resolver } from 'type-graphql';
+import { isAuthenticated } from '../middlewares/isAuthenticated';
+import Product from '../entities/Product';
+import {
+  Arg,
+  Ctx,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+  UseMiddleware,
+} from 'type-graphql';
+import { MyContext } from '../apollo/createApolloServer';
+import UserData from '../entities/UserData';
 
 type Category = 'emoji' | 'figures' | 'pad';
 type Condition = 'new' | 'used';
 @Resolver(Product)
 export class ProductResolver {
   @Query(() => [Product])
-  products(): Product[] {
-    return dummyProducts.products;
+  async products(): Promise<Product[]> {
+    return await Product.find({ relations: ['owner'] });
   }
 
-  @Query(() => Product, { nullable: true })
-  product(@Arg('productId', () => Int) productId: number): Product | undefined {
-    return dummyProducts.products.find((product) => product.id === productId);
+  @Query(() => Product)
+  async product(
+    @Arg('productId', () => Int) productId: number,
+  ): Promise<Product | undefined> {
+    return await Product.findOne({
+      where: { id: productId },
+      relations: ['owner'],
+    });
   }
 
   @Query(() => [Product])
-  searchProducts(
+  async searchProducts(
     @Arg('category', { nullable: true })
     category?: Category,
     @Arg('conditions', () => [String], { nullable: true })
     conditions?: Condition[],
-  ): Product[] | [] {
+  ): Promise<Product[] | []> {
+    let products = await Product.find();
+
     if (category) {
-      const products = dummyProducts.products.filter(
-        (product) => product.category === category,
-      );
-      return conditions && conditions.length > 0
-        ? products.filter((product) =>
-            conditions.includes(product.condition as Condition),
-          )
-        : products;
+      products = products.filter((product) => product.category === category);
     }
-    return conditions && conditions.length > 0
-      ? dummyProducts.products.filter((product) =>
-          conditions.includes(product.condition as Condition),
-        )
-      : dummyProducts.products;
+
+    if (conditions && conditions.length > 0) {
+      products = products.filter((product) =>
+        conditions.includes(product.condition as Condition),
+      );
+    }
+
+    return products;
+  }
+
+  @Mutation(() => Product)
+  @UseMiddleware(isAuthenticated)
+  async registSale(
+    @Arg('title') title: string,
+    @Arg('category') category: Category,
+    @Arg('description') description: string,
+    @Arg('imageUrl') imageUrl: string,
+    @Arg('blurDataUrl') blurDataUrl: string,
+    @Arg('price') price: number,
+    @Arg('condition') condition: Condition,
+    @Ctx() { verifiedUser }: MyContext,
+  ): Promise<Product> {
+    const user = await UserData.findOne({ where: { id: verifiedUser.userId } });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const product = Product.create({
+      title,
+      category,
+      description,
+      imageUrl,
+      blurDataUrl,
+      price,
+      condition,
+      owner: user,
+    });
+
+    await product.save();
+
+    return product;
   }
 }

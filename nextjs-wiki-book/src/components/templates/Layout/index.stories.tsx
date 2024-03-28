31f00b73aa2cb438f24c7800757f0407146db42a
kmw <kmw9708@gmail.com>
2024-03-28 23:05:46 +0900

    test: template 스토리북 수정

--- nextjs-wiki-book/src/components/templates/Layout/index.stories.tsx
+++ nextjs-wiki-book/src/components/templates/Layout/index.stories.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { MockedProvider } from '@apollo/client/testing';
 import { Meta, StoryObj } from '@storybook/react';
 import Layout from './';
 import ProductForm from '@/components/organisms/ProductForm';
@@ -20,13 +22,25 @@ export default meta;
 type Story = StoryObj<typeof Layout>;
 
 const Template: Story = {
-  render: ({ children }) => <Layout>{children}</Layout>,
+  render: ({ children }) => (
+    <MockedProvider>
+      <Layout>{children}</Layout>
+    </MockedProvider>
+  ),
+};
+
+const handleSignIn = (): Promise<any> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ success: true, message: '로그인이 성공했습니다.' });
+    }, 1000);
+  });
 };
 
 export const SigninPage = {
   ...Template,
   args: {
-    children: <SignInForm />,
+    children: <SignInForm onSignIn={handleSignIn} isLoading={false} />,
   },
 };
 

f16e4f42a6a352cf98c352c30a290aea67232edf
kmw <kmw9708@gmail.com>
2024-03-28 22:45:58 +0900

    test: 헤더 graphql 사용으로 인한 수정

--- nextjs-wiki-book/src/components/organisms/Header/index.spec.tsx
+++ nextjs-wiki-book/src/components/organisms/Header/index.spec.tsx
@@ -1,137 +1,54 @@
+import { gql } from '@apollo/client';
+import { MockedProvider } from '@apollo/client/testing';
 import { RenderResult, render, screen } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import Header from './';
-import { AuthContextProvider } from '@/contexts/AuthContext';
-import { useShoppingCartContext } from '@/contexts/ShoppingCartContext';
 import { theme } from '@/styles/themes';
-import { Product, User } from '@/types';
 
-jest.mock('contexts/shoppingCartContext');
-const { ShoppingCartContextProvider } = jest.requireActual(
-  'contexts/ShoppingCartContext',
-);
-
-// 더미 사용자
-const authUser: User = {
-  id: 1,
-  username: 'T.S',
-  displayName: 'Test User',
-  email: 'test@test.com',
-  description: '',
-  profileImageUrl: '/images/sample/1.jpg',
-};
-
-// 더미 상품
-const product: Product = {
-  id: 1,
-  category: 'emoji',
-  title: 'Product',
-  description: '',
-  imageUrl: '/images/sample/1.jpg',
-  blurDataUrl: '',
-  price: 3000,
-  condition: 'new',
-  owner: authUser,
-};
-
-const products: Product[] = [
+const mocks = [
   {
-    id: 1,
-    category: 'emoji',
-    title: 'Product',
-    description: '',
-    imageUrl: '/images/sample/1.jpg',
-    blurDataUrl: '',
-    price: 3000,
-    condition: 'new',
-    owner: authUser,
-  },
-  {
-    id: 2,
-    category: 'emoji',
-    title: 'Product',
-    description: '',
-    imageUrl: '/images/sample/2.jpg',
-    blurDataUrl: '',
-    price: 5000,
-    condition: 'new',
-    owner: authUser,
+    request: {
+      query: gql`
+        query me {
+          me {
+            id
+            username
+            email
+            displayName
+            profileImageUrl
+            description
+            createdAt
+            updatedAt
+          }
+        }
+      `,
+    },
+    result: {
+      data: {
+        me: {
+          id: 1,
+          username: 'mock',
+          email: 'mock',
+          displayName: 'mock',
+          profileImageUrl: '/images/sample/1.jpg',
+          description: '',
+          createdAt: '',
+          updatedAt: '',
+        },
+      },
+    },
   },
 ];
 
 describe('Header', () => {
   let renderResult: RenderResult;
-  const useShoppingCartContextMock =
-    useShoppingCartContext as jest.MockedFunction<
-      typeof useShoppingCartContext
-    >;
-
-  it('카트에 상품이 존재한다.', async () => {
-    useShoppingCartContextMock.mockReturnValue({
-      cart: [product],
-      addProductToCart: () => {},
-      removeProductFromCart: () => {},
-    });
-
-    renderResult = render(
-      <ThemeProvider theme={theme}>
-        <ShoppingCartContextProvider>
-          <AuthContextProvider
-            authUser={authUser}
-            context={{ apiRootUrl: 'https://dummy' }}
-          >
-            <Header />
-          </AuthContextProvider>
-        </ShoppingCartContextProvider>
-      </ThemeProvider>,
-    );
-    // 카트에 상품 존재시 배지가 표시된다.
-    expect(screen.getAllByTestId('badge-wrapper').length).toBeGreaterThan(0);
-
-    renderResult.unmount();
-    useShoppingCartContextMock.mockReset();
-  });
-
-  it('카트에 상품이 2개 존재한다.', async () => {
-    useShoppingCartContextMock.mockReturnValue({
-      cart: [...products],
-      addProductToCart: () => {},
-      removeProductFromCart: () => {},
-    });
-
-    renderResult = render(
-      <ThemeProvider theme={theme}>
-        <ShoppingCartContextProvider>
-          <AuthContextProvider
-            authUser={authUser}
-            context={{ apiRootUrl: 'https/dummy' }}
-          >
-            <Header />
-          </AuthContextProvider>
-        </ShoppingCartContextProvider>
-      </ThemeProvider>,
-    );
-
-    expect(screen.queryAllByTestId('badge-wrapper')[0].textContent).toBe('2');
-
-    renderResult.unmount();
-    useShoppingCartContextMock.mockReset();
-  });
 
   it('비 로그인', async () => {
-    useShoppingCartContextMock.mockReturnValue({
-      cart: [],
-      addProductToCart: () => {},
-      removeProductFromCart: () => {},
-    });
-
     renderResult = render(
       <ThemeProvider theme={theme}>
-        <ShoppingCartContextProvider>
-          <AuthContextProvider context={{ apiRootUrl: 'https://dummy ' }}>
-            <Header />
-          </AuthContextProvider>
-        </ShoppingCartContextProvider>
+        <MockedProvider mocks={mocks}>
+          <Header />
+        </MockedProvider>
       </ThemeProvider>,
     );
 
@@ -142,6 +59,5 @@ describe('Header', () => {
     expect(screen.queryByTestId('badge-wrapper')).toBeNull();
 
     renderResult.unmount();
-    useShoppingCartContextMock.mockReset();
   });
 });
--- nextjs-wiki-book/src/components/organisms/Header/index.stories.tsx
+++ nextjs-wiki-book/src/components/organisms/Header/index.stories.tsx
@@ -1,12 +1,7 @@
+import { gql } from '@apollo/client';
+import { MockedProvider } from '@apollo/client/testing';
 import { Meta } from '@storybook/react';
-import { useEffect } from 'react';
 import Header from './';
-import { AuthContextProvider } from '@/contexts/AuthContext';
-import {
-  ShoppingCartContextProvider,
-  useShoppingCartContext,
-} from '@/contexts/ShoppingCartContext';
-import { User } from '@/types';
 
 const meta: Meta<typeof Header> = {
   title: 'Organisms/Header',
@@ -14,47 +9,33 @@ const meta: Meta<typeof Header> = {
 
 export default meta;
 
-// 로그인 x
-export const NoLogin = () => <Header />;
-
-// 로그인 o
-export const Login = () => {
-  const authSampleUser: User = {
-    id: 1,
-    username: 'dummy',
-    displayName: 'KMW',
-    email: 'mw.k@example.com',
-    profileImageUrl: '/images/sample/1.jpg',
-    description: '',
-  };
-
-  const ChildComponent = () => {
-    const { addProductToCart } = useShoppingCartContext();
-    // 더미 상품
-    useEffect(() => {
-      addProductToCart({
-        id: 1,
-        category: 'emoji',
-        title: 'Product',
-        description: '',
-        imageUrl: '/images/sample/1.jpg',
-        blurDataUrl: '',
-        price: 1000,
-        condition: 'used',
-        owner: authSampleUser,
-      });
-    }, []);
-    return <Header />;
-  };
-
+export const NoLogin = () => {
+  const mock = [
+    {
+      request: {
+        query: gql`
+          query me {
+            me {
+              id
+              username
+              email
+              displayName
+              profileImageUrl
+              description
+              createdAt
+              updatedAt
+            }
+          }
+        `,
+      },
+      result: {
+        data: null,
+      },
+    },
+  ];
   return (
-    <ShoppingCartContextProvider>
-      <AuthContextProvider
-        context={{ apiRootUrl: 'https://dummy' }}
-        authUser={authSampleUser}
-      >
-        <ChildComponent />
-      </AuthContextProvider>
-    </ShoppingCartContextProvider>
+    <MockedProvider mocks={mock}>
+      <Header />
+    </MockedProvider>
   );
 };

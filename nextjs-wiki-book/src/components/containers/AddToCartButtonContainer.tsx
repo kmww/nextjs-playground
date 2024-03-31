c6d679ba0070e917ed65795668b5d584e2131f06
kmw <kmw9708@gmail.com>
2024-04-01 00:08:03 +0900

    feat: 장바구니 추가 알림

--- nextjs-wiki-book/src/components/containers/AddToCartButtonContainer.tsx
+++ nextjs-wiki-book/src/components/containers/AddToCartButtonContainer.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/atoms/Button';
+import { useGlobalSpinnerActionsContext } from '@/contexts/GlobalSpinnerContext';
 import { useAddTocartMutation } from '@/generated/graphql';
 import { Product } from '@/types';
 
@@ -10,14 +11,25 @@ const AddToCartButtonContainer = ({
   product,
 }: AddToCartButtonContainerProps) => {
   const [addToCart] = useAddTocartMutation();
+  const setGlobalSpinner = useGlobalSpinnerActionsContext();
 
-  const handleAddToCartButtonClick = () => {
-    const productId = Number(product.id);
-    addToCart({
-      variables: {
-        cartItemInput: { productId: productId, quantity: 1 },
-      },
-    });
+  const handleAddToCartButtonClick = async () => {
+    try {
+      setGlobalSpinner(true);
+      const productId = Number(product.id);
+      await addToCart({
+        variables: {
+          cartItemInput: { productId: productId, quantity: 1 },
+        },
+      });
+      window.alert('장바구니에 상품이 담겼습니다!');
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    } finally {
+      setGlobalSpinner(false);
+    }
   };
 
   return (

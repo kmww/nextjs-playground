aea8dc8f4291c542a6b144cacf91a07206a873b1
kmw <kmw9708@gmail.com>
2024-04-05 19:57:20 +0900

    feat: 404페이지 메인버튼 추가 및 스타일 적용

--- nextjs-wiki-book/src/pages/404.tsx
+++ nextjs-wiki-book/src/pages/404.tsx
@@ -1,10 +1,74 @@
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import BackgroundImage from '@/components/atoms/BackgroundImage';
+import Button from '@/components/atoms/Button';
 import Text from '@/components/atoms/Text';
+import Flex from '@/components/layout/Flex';
+import Layout from '@/components/templates/Layout';
+
+const BackgroundWrapper = styled.div`
+  position: relative;
+  width: 300px;
+  height: 300px;
+  margin-top: 40px;
+`;
+
+const BackButton = styled(Button)`
+  background-color: transparent;
+  text-decoration: underline;
+  font-size: large;
+  border: 1px solid;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondaryLight};
+  }
+`;
 
 const NotFoundPage = () => {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push('/');
+  };
+
   return (
-    <Text as="h1" variant="extraLarge">
-      404- Page Not Found
-    </Text>
+    <Layout>
+      <Flex justifyContent="center">
+        <Flex flexDirection="column" alignItems="center">
+          <BackgroundWrapper>
+            <BackgroundImage
+              imageUrl="/bg/404.png"
+              width="100%"
+              height="100%"
+              style={{ zIndex: 1 }}
+            />
+          </BackgroundWrapper>
+          <Text
+            as="h1"
+            variant="extraLarge"
+            fontWeight="bold"
+            textAlign="center"
+            wordBreak="keep-all"
+            textWrap="wrap"
+          >
+            페이지를 찾을 수 없습니다.
+          </Text>
+          <Text
+            as="p"
+            variant="medium"
+            fontWeight="bold"
+            textAlign="center"
+            wordBreak="keep-all"
+            textWrap="wrap"
+            marginTop={0}
+          >
+            존재하지 않는 주소를 입력하셨거나, <br />
+            요청하신 페이지의 주소가 변경/삭제 되어 찾을 수 없습니다.
+          </Text>
+          <BackButton onClick={handleRedirect}>메인으로</BackButton>
+        </Flex>
+      </Flex>
+    </Layout>
   );
 };
 

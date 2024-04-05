5f66f865a37914598bcb48da00b40c5159aaefbc
kmw <kmw9708@gmail.com>
2024-04-05 19:54:22 +0900

    style: 배경색상

--- nextjs-wiki-book/src/components/templates/Layout/index.tsx
+++ nextjs-wiki-book/src/components/templates/Layout/index.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import Separator from '@/components/atoms/Sparator';
 import Box from '@/components/layout/Box';
 import Footer from '@/components/organisms/Footer';
@@ -7,10 +8,28 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
+export const StyledBox = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+`;
+
+const ImageBox = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #f6f6e5;
+`;
+
 const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
+      <StyledBox>
+        <ImageBox />
+      </StyledBox>
       <main>{children}</main>
       <Separator />
       <Box padding={3}>

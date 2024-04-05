2819d0e460dcd9b85071c12f724c221b4b85cf1c
kmw <kmw9708@gmail.com>
2024-04-05 19:55:03 +0900

    style: 로그인 스타일 변경

--- nextjs-wiki-book/src/pages/signin.tsx
+++ nextjs-wiki-book/src/pages/signin.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from 'next';
-import AppLogo from '@/components/atoms/AppLogo';
+import BackgroundImage from '@/components/atoms/BackgroundImage';
+import Text from '@/components/atoms/Text';
 import SignInFormContainer from '@/components/containers/SignInFormContainer';
 import Box from '@/components/layout/Box';
 import Flex from '@/components/layout/Flex';
+import Wrapper from '@/components/layout/Wrapper';
 import Layout from '@/components/templates/Layout';
 
 const SigninPage: NextPage = () => {
@@ -15,19 +17,30 @@ const SigninPage: NextPage = () => {
         paddingLeft={{ base: 2, md: 0 }}
         justifyContent="center"
       >
-        <Flex
-          width="400px"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
+        <Wrapper
+          width="500px"
+          height="500px"
+          backgroundColor="secondaryLightOp"
         >
-          <Box marginBottom={2}>
-            <AppLogo />
-          </Box>
+          <BackgroundImage
+            imageUrl="/bg/FooterBG.png"
+            width="100%"
+            height="38%"
+            bottom="0"
+            right="0"
+          />
           <Box width="100%">
+            <Box marginBottom={1}>
+              <Text
+                fontSize="40px"
+                style={{ fontFamily: 'fantasy, sans-serif' }}
+              >
+                LOA WIKI
+              </Text>
+            </Box>
             <SignInFormContainer />
           </Box>
-        </Flex>
+        </Wrapper>
       </Flex>
     </Layout>
   );

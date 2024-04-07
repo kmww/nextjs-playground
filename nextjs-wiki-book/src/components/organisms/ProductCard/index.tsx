d7925a17bf421e275f15f0df18f6f64296a20ed6
kmw <kmw9708@gmail.com>
2024-04-07 20:44:27 +0900

    refactor: hover추가, 제목/가격 길이 설정

--- nextjs-wiki-book/src/components/organisms/ProductCard/index.tsx
+++ nextjs-wiki-book/src/components/organisms/ProductCard/index.tsx
@@ -1,6 +1,8 @@
+import styled from 'styled-components';
 import ScaleImage from '@/components/atoms/ScaleImage';
 import Text from '@/components/atoms/Text';
 import Box from '@/components/layout/Box';
+import Flex from '@/components/layout/Flex';
 
 interface ProductCardProps {
   title: string;
@@ -9,6 +11,14 @@ interface ProductCardProps {
   blurDataUrl?: string | null;
   variant?: 'listing' | 'small' | 'detail';
 }
+
+const StyledProductCard = styled(Flex)`
+  transition: transform 0.5s linear;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover {
+    transform: scale(1.03);
+  }
+`;
 
 const ProductCard = ({
   title,
@@ -20,16 +30,23 @@ const ProductCard = ({
   const { size, imgSize } = (() => {
     switch (variant) {
       case 'detail':
-        return { size: { base: '320px', md: '540px' }, imgSize: 540 };
+        return { size: { base: '250px', md: '300px' }, imgSize: 540 };
       case 'listing':
         return { size: { base: '160px', md: '240px' }, imgSize: 240 };
       default:
-        return { size: { base: '300px' }, imgSize: 300 };
+        return { size: { base: '250px', md: '300px' }, imgSize: 300 };
     }
   })();
 
   return (
-    <>
+    <StyledProductCard
+      flexDirection="column"
+      backgroundColor="secondaryLight"
+      width={size}
+      padding={2}
+      margin={1}
+      style={{ borderRadius: '10px', boxSizing: 'content-box' }}
+    >
       {blurDataUrl && (
         <ScaleImage
           src={imageUrl}
@@ -53,16 +70,31 @@ const ProductCard = ({
         />
       )}
       {variant === 'small' && (
-        <Box marginTop={1}>
-          <Text as="h2" variant="medium" margin={0} padding={0}>
+        <Box marginTop={1} width="250px">
+          <Text
+            as="h2"
+            variant="mediumLarge"
+            height="26px"
+            margin={0}
+            overflow="hidden"
+            textOverflow="ellipsis"
+            whiteSpace="nowrap"
+          >
             {title}
           </Text>
-          <Text as="span" variant="medium">
+          <Text
+            as="span"
+            variant="medium"
+            display="block"
+            overflow="hidden"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+          >
             {price}원
           </Text>
         </Box>
       )}
-    </>
+    </StyledProductCard>
   );
 };
 

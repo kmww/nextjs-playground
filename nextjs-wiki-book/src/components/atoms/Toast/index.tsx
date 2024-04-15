e84b33a992f9313fcd87a1ea7b721fd21cd0dce5
kmw <kmw9708@gmail.com>
2024-04-15 21:47:25 +0900

    feat: Toast progress bar 구현

--- nextjs-wiki-book/src/components/atoms/Toast/index.tsx
+++ nextjs-wiki-book/src/components/atoms/Toast/index.tsx
@@ -13,12 +13,42 @@ const StyledToast = styled(Box)`
   padding-top: 10px;
 `;
 
+const Progress = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 5px;
+  border-radius: 0 0 5px 0;
+  padding: 0;
+  background: rgba(255, 255, 255, 0.1);
+`;
+
+const ProgressValue = styled.div<{ duration: number }>`
+  height: 5px;
+  background: ${({ theme }) => theme.colors.secondaryDark};
+  border-radius: 0 0 5px 0;
+  box-shadow: 0 1px 1px -1px ${({ theme }) => theme.colors.secondaryDark};
+  animation: load ${({ duration }) => `${duration}ms`} normal forwards;
+
+  @keyframes load {
+    0% {
+      width: 0;
+    }
+    100% {
+      width: 100%;
+    }
+  }
+`;
+
 interface ToastProps {
   content: string;
+  duration: number;
 }
 
 const Toast = (props: ToastProps) => {
-  const { content = '예시 입력' } = props;
+  const { content = '예시 입력', duration } = props;
 
   return (
     <>
@@ -26,6 +56,9 @@ const Toast = (props: ToastProps) => {
         <Text textAlign="center" padding={1} fontWeight="bold">
           {content}
         </Text>
+        <Progress>
+          <ProgressValue duration={duration} />
+        </Progress>
       </StyledToast>
     </>
   );

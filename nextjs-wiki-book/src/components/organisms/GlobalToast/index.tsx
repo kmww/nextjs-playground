ef94d4d73de944b9a1a1e98e5a7df3ae4559fd34
kmw <kmw9708@gmail.com>
2024-04-15 21:49:26 +0900

    feat: GlobalToast 애니메이션 설정

--- nextjs-wiki-book/src/components/organisms/GlobalToast/index.tsx
+++ nextjs-wiki-book/src/components/organisms/GlobalToast/index.tsx
@@ -16,6 +16,26 @@ const ToastWrapper = styled.div<{
   right: 0;
   left: 0;
   bottom: 0;
+  animation: fadeInOut ${({ duration }) => duration + 100}ms ease;
+  backface-visibility: hidden;
+  z-index: 2;
+
+  @keyframes fadeInOut {
+    0% {
+      opacity: 0.1;
+    }
+    30% {
+      opacity: 1;
+    }
+    90% {
+      opacity: 0.9;
+      transform: rotateX(10deg);
+    }
+    100% {
+      opacity: 0;
+      transform: rotateX(90deg);
+    }
+  }
 `;
 
 interface GlobalToast {

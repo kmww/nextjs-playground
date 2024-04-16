839c06fc2c5b57c917dcdd9ee3ae320168b14286
kmw <kmw9708@gmail.com>
2024-04-16 19:28:01 +0900

    refactor: globalToast recoil 상태 변경

--- nextjs-wiki-book/src/contexts/GlobalToast/globalToast.ts
+++ nextjs-wiki-book/src/contexts/GlobalToast/globalToast.ts
@@ -1,6 +1,13 @@
 import { atom } from 'recoil';
 
-export const globalToast = atom<[boolean, string]>({
+/**
+ * Toast State
+ * @param {boolean} isToastOn - 토스트를 보여줄지 여부
+ * @param {string} content - 토스트 내용
+ * @param {string}  variant - 토스트 상태
+ * @default - [false, '내용을 입력하세요', 'primary']
+ */
+export const globalToast = atom<[boolean, ...string[]]>({
   key: 'isGlobalToastOn',
-  default: [false, ''],
+  default: [false, '내용을 입력하세요', 'primary'],
 });

abbeca42284b03d8585b524c4de469b19bfde2df
kmw <kmw9708@gmail.com>
2024-04-18 14:53:23 +0900

    feat: 로그인 toast 설정

--- nextjs-wiki-book/src/components/containers/SignInFormContainer.tsx
+++ nextjs-wiki-book/src/components/containers/SignInFormContainer.tsx
@@ -3,12 +3,15 @@ import { useSetRecoilState } from 'recoil';
 import SignInForm from '@/components/organisms/SignInForm';
 import { isLoggedInState } from '@/contexts/Auth/auth';
 import { globalSpinner } from '@/contexts/GlobalSpinner/globalSpinner';
+import { globalToast } from '@/contexts/GlobalToast/globalToast';
 import { LoginInput, useLoginMutation } from '@/generated/graphql';
 
 const SignInFormContainer = () => {
   const [login, { loading }] = useLoginMutation();
   const setIsLoggedIn = useSetRecoilState(isLoggedInState);
   const setGlobalSpinner = useSetRecoilState(globalSpinner);
+  const setToast = useSetRecoilState(globalToast);
+
   const router = useRouter();
   const handleSignin = async (loginInput: LoginInput) => {
     try {
@@ -18,16 +21,16 @@ const SignInFormContainer = () => {
       if (res.data?.login.user) {
         const redirectTo = (router.query['redirect_to'] as string) ?? '/';
         setIsLoggedIn(true);
+        setToast([true, '로그인이 완료되었습니다.', 'primary']);
         router.push(redirectTo);
 
         return res;
       } else if (res.data?.login.errors) {
-        window.alert(res.data.login.errors[0].message);
+        setToast([true, res.data.login.errors[0].message, 'danger']);
       }
     } catch (err: unknown) {
       if (err instanceof Error) {
-        window.alert(`로그인 도중 문제가 발생했습니다.
-errors:${err.message}`);
+        setToast([true, '로그인 도중 문제가 발생했습니다.', 'danger']);
       }
     } finally {
       setGlobalSpinner(false);

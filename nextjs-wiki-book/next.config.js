8ff6562671bab775b0a5fd82b65bdece4a4d8c70
kmw <kmw9708@gmail.com>
2024-04-26 20:14:43 +0900

    fix: 도메인 수정

--- nextjs-wiki-book/next.config.js
+++ nextjs-wiki-book/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       'cdn-lostark.game.onstove.com',
       'd3kxs6kpbh59hp.cloudfront.net',
       'd2x8kymwjom7h7.cloudfront.net',
-      process.env.IMAGE,
+      process.env.NEXT_PUBLIC_IMAGE,
     ],
   },
   compiler: (() => {

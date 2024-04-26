abe84dd295155d8f3a21c0189f743c06a95993a9
kmw <kmw9708@gmail.com>
2024-04-26 15:20:40 +0900

    feat: redis-client 수정

--- nextjs-wiki-server/src/redis/redis-client.ts
+++ nextjs-wiki-server/src/redis/redis-client.ts
@@ -3,6 +3,8 @@ import { Redis } from 'ioredis';
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: Number(process.env.REDIS_PROT) || 6379,
+  username: process.env.REDIS_NAME || 'default',
+  password: process.env.REDIS_PASSWORD || 'qwer1234',
 });
 
 export default redis;

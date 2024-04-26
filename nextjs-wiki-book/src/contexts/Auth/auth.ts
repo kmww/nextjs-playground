1ef6c2cdfb56c56189cf4d078883a95478ee2279
kmw <kmw9708@gmail.com>
2024-04-26 15:20:06 +0900

    bug: recoil uuid 임시제거

--- nextjs-wiki-book/src/contexts/Auth/auth.ts
+++ nextjs-wiki-book/src/contexts/Auth/auth.ts
@@ -1,4 +1,3 @@
-import uuid from 'react-uuid';
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 import { MeQuery } from '@/generated/graphql';
@@ -7,28 +6,28 @@ const localStorage =
   typeof window !== 'undefined' ? window.localStorage : undefined;
 
 const { persistAtom } = recoilPersist({
-  key: `recoil-persist/${uuid()}`,
+  key: `recoil-persist`,
   storage: localStorage,
 });
 
 const { persistAtom: persistProfileImageUrl } = recoilPersist({
-  key: `profileImage/${uuid()}`,
+  key: `profileImage`,
   storage: localStorage,
 });
 
 export const isLoggedInState = atom<boolean>({
-  key: `isLoggedIn/${uuid()}`,
+  key: `isLoggedIn`,
   default: localStorage?.getItem('access_toke') ? true : false,
   effects: [persistAtom],
 });
 
 export const profileImageUrlState = atom<string | null | undefined>({
-  key: `profileImageUrlstate/${uuid()}`,
+  key: `profileImageUrlstate`,
   default: '',
   effects: [persistProfileImageUrl],
 });
 
 export const userData = atom<MeQuery | undefined>({
-  key: `useData/${uuid()}`,
+  key: `useData`,
   default: {},
 });

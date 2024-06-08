d84779cff1403822307e71e5b9575fd81d076f7b
kmw <kmw9708@gmail.com>
2024-06-08 16:03:45 +0900

    remove: 프로필, 로그인관련 recoilstate 제거

--- nextjs-wiki-book/src/contexts/Auth/auth.ts
+++ nextjs-wiki-book/src/contexts/Auth/auth.ts
@@ -1,31 +1,5 @@
 import { atom } from 'recoil';
-import { recoilPersist } from 'recoil-persist';
 import { MeQuery } from '@/generated/graphql';
-
-const localStorage =
-  typeof window !== 'undefined' ? window.localStorage : undefined;
-
-const { persistAtom } = recoilPersist({
-  key: `recoil-persist`,
-  storage: localStorage,
-});
-
-const { persistAtom: persistProfileImageUrl } = recoilPersist({
-  key: `profileImage`,
-  storage: localStorage,
-});
-
-export const isLoggedInState = atom<boolean>({
-  key: `isLoggedIn`,
-  default: localStorage?.getItem('access_toke') ? true : false,
-  effects: [persistAtom],
-});
-
-export const profileImageUrlState = atom<string | null | undefined>({
-  key: `profileImageUrlstate`,
-  default: '',
-  effects: [persistProfileImageUrl],
-});
 
 export const userData = atom<MeQuery | undefined>({
   key: `useData`,
